3cb6e789c284d083b890422d5923c0789d7473d4
Al Calzone <d.griesel@gmx.net>
2018-05-06 18:05:07 +0200

    Implement MultilevelSwitchCC V4

---
+++ build/lib/commandclass/MultilevelSwitchCC.d.ts
@@ -0,0 +1,51 @@
+/// <reference types="node" />
+import { CommandClass } from "./CommandClass";
+export declare enum MultilevelSwitchCommand {
+    Set = 1,
+    Get = 2,
+    Report = 3,
+    StartLevelChange = 4,
+    StopLevelChange = 5,
+    SupportedGet = 6,
+    SupportedReport = 7,
+}
+export declare class MultilevelSwitchCC extends CommandClass {
+    nodeId: number;
+    ccCommand: MultilevelSwitchCommand;
+    constructor(nodeId?: number);
+    constructor(nodeId: number, ccCommand: MultilevelSwitchCommand.Get);
+    constructor(nodeId: number, ccCommand: MultilevelSwitchCommand.Set, targetValue: number, duration?: number);
+    constructor(nodeId: number, ccCommand: MultilevelSwitchCommand.StartLevelChange, direction: keyof typeof LevelChangeDirection, ignoreStartLevel: boolean, startLevel: number, duration?: number, secondarySwitchDirection?: keyof typeof LevelChangeDirection);
+    constructor(nodeId: number, ccCommand: MultilevelSwitchCommand.StopLevelChange);
+    constructor(nodeId: number, ccCommand: MultilevelSwitchCommand.SupportedGet);
+    targetValue: number;
+    duration: number;
+    direction: keyof typeof LevelChangeDirection;
+    secondarySwitchDirection: keyof typeof LevelChangeDirection;
+    ignoreStartLevel: boolean;
+    startLevel: number;
+    secondarySwitchStepSize: number;
+    private _primarySwitchType;
+    readonly primarySwitchType: SwitchType;
+    private _secondarySwitchType;
+    readonly secondarySwitchType: SwitchType;
+    private _currentValue;
+    readonly currentValue: number;
+    serialize(): Buffer;
+    deserialize(data: Buffer): void;
+}
+export declare enum LevelChangeDirection {
+    "up" = 0,
+    "down" = 1,
+    "none" = 3,
+}
+export declare enum SwitchType {
+    "not supported" = 0,
+    "Off/On" = 1,
+    "Down/Up" = 2,
+    "Close/Open" = 3,
+    "CCW/CW" = 4,
+    "Left/Right" = 5,
+    "Reverse/Forward" = 6,
+    "Pull/Push" = 7,
+}
--- src/lib/commandclass/MultilevelSwitchCC.ts
+++ src/lib/commandclass/MultilevelSwitchCC.ts
@@ -12,7 +12,7 @@ export enum MultilevelSwitchCommand {
 }
 
 @commandClass(CommandClasses["Multilevel Switch"])
-@implementedVersion(1)
+@implementedVersion(4)
 @expectedCCResponse(CommandClasses["Multilevel Switch"])
 export class MultilevelSwitchCC extends CommandClass {
 
@@ -22,16 +22,23 @@ export class MultilevelSwitchCC extends CommandClass {
 	constructor(
 		nodeId: number,
 		ccCommand: MultilevelSwitchCommand.Set,
-		value: number,
+		targetValue: number,
+		// Version >= 2:
+		duration?: number,
 	);
 	constructor(
 		nodeId: number,
 		ccCommand: MultilevelSwitchCommand.StartLevelChange,
-		direction: LevelChangeDirection,
+		direction: keyof typeof LevelChangeDirection,
 		ignoreStartLevel: boolean,
 		startLevel: number,
+		// Version >= 2:
+		duration?: number,
+		// Version >= 3:
+		secondarySwitchDirection?: keyof typeof LevelChangeDirection,
 	);
 	constructor(nodeId: number, ccCommand: MultilevelSwitchCommand.StopLevelChange);
+	constructor(nodeId: number, ccCommand: MultilevelSwitchCommand.SupportedGet);
 
 	constructor(
 		public nodeId: number,
@@ -39,17 +46,88 @@ export class MultilevelSwitchCC extends CommandClass {
 		...args: any[],
 	) {
 		super(nodeId);
+		if (ccCommand === MultilevelSwitchCommand.Set) {
+			[this.targetValue, this.duration] = args;
+		} else if (ccCommand === MultilevelSwitchCommand.StartLevelChange) {
+			[
+				this.direction,
+				this.ignoreStartLevel,
+				this.startLevel,
+				this.duration,
+				this.secondarySwitchDirection,
+			] = args;
+		}
 	}
 	// tslint:enable:unified-signatures
 
+	public targetValue: number;
+	public duration: number;
+	public direction: keyof typeof LevelChangeDirection;
+	public secondarySwitchDirection: keyof typeof LevelChangeDirection;
+	public ignoreStartLevel: boolean;
+	public startLevel: number;
+	public secondarySwitchStepSize: number;
+
+	private _primarySwitchType: SwitchType;
+	public get primarySwitchType(): SwitchType {
+		return this._primarySwitchType;
+	}
+
+	private _secondarySwitchType: SwitchType;
+	public get secondarySwitchType(): SwitchType {
+		return this._secondarySwitchType;
+	}
+
+	private _currentValue: number;
+	public get currentValue(): number {
+		return this._currentValue;
+	}
+
 	public serialize(): Buffer {
 		switch (this.ccCommand) {
-			// case MultilevelSwitchCommand.TODO:
-			// 	// serialize payload
-			// 	break;
+			case MultilevelSwitchCommand.Set: {
+				const payload = [this.ccCommand, this.targetValue];
+				if (this.version >= 2) {
+					payload.push(this.duration);
+				}
+				this.payload = Buffer.from(payload);
+				break;
+			}
+
+			case MultilevelSwitchCommand.StartLevelChange: {
+				let controlByte =
+					(LevelChangeDirection[this.direction] << 6)
+					| (this.ignoreStartLevel ? 0b0010_0000 : 0)
+					;
+				if (this.version >= 3) {
+					if (this.secondarySwitchDirection != null) {
+						controlByte |= LevelChangeDirection[this.secondarySwitchDirection] << 3;
+					}
+				}
+				const payload = [
+					this.ccCommand,
+					controlByte,
+					this.startLevel,
+				];
+				if (this.version >= 2) {
+					payload.push(this.duration);
+				}
+				if (this.version >= 3) {
+					payload.push(this.secondarySwitchStepSize);
+				}
+				this.payload = Buffer.from(payload);
+			}
+
+			case MultilevelSwitchCommand.Get:
+			case MultilevelSwitchCommand.StopLevelChange:
+			case MultilevelSwitchCommand.SupportedGet:
+				// no actual payload
+				this.payload = Buffer.from([this.ccCommand]);
+				break;
+
 			default:
 				throw new ZWaveError(
-					"Cannot serialize a MultilevelSwitch CC with a command other than __TODO__",
+					"Cannot serialize a MultilevelSwitch CC with a command other than Set, Get, StartLevelChange, StopLevelChange, SupportedGet",
 					ZWaveErrorCodes.CC_Invalid,
 				);
 		}
@@ -62,13 +140,22 @@ export class MultilevelSwitchCC extends CommandClass {
 
 		this.ccCommand = this.payload[0];
 		switch (this.ccCommand) {
-			// case MultilevelSwitchCommand.TODO:
-			// 	// parse payload
-			// 	break;
+			case MultilevelSwitchCommand.Report:
+				[
+					this._currentValue,
+					this.targetValue,
+					this.duration,
+				] = this.payload.slice(1);
+				break;
+
+			case MultilevelSwitchCommand.SupportedReport:
+				this._primarySwitchType = this.payload[1] & 0b11111;
+				this._secondarySwitchType = this.payload[2] & 0b11111;
+				break;
 
 			default:
 				throw new ZWaveError(
-					"Cannot deserialize a MultilevelSwitch CC with a command other than TODO",
+					"Cannot deserialize a MultilevelSwitch CC with a command other than Report",
 					ZWaveErrorCodes.CC_Invalid,
 				);
 		}
@@ -76,4 +163,19 @@ export class MultilevelSwitchCC extends CommandClass {
 
 }
 
-export type LevelChangeDirection = "up" | "down";
+export enum LevelChangeDirection {
+	"up" = 0b0,
+	"down" = 0b1,
+	"none" = 0b11,
+}
+
+export enum SwitchType {
+	"not supported" = 0x00,
+	"Off/On" = 0x01,
+	"Down/Up" = 0x02,
+	"Close/Open" = 0x03,
+	"CCW/CW" = 0x04,
+	"Left/Right" = 0x05,
+	"Reverse/Forward" = 0x06,
+	"Pull/Push" = 0x07,
+}
