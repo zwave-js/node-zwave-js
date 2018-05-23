bbb7a7558945325a563e2625c7528b58593b40f5
dominic.griesel <d.griesel@gmx.net>
2018-05-23 13:39:57 +0200

    BasicCC: Add tests

---
+++ src/lib/commandclass/BasicCC.test.ts
@@ -0,0 +1,93 @@
+// tslint:disable:no-unused-expression
+
+import { expect, should } from "chai";
+import { stub } from "sinon";
+should();
+
+import { ZWaveError, ZWaveErrorCodes } from "../error/ZWaveError";
+import { BasicCC, BasicCommand } from "./BasicCC";
+import { CommandClasses } from "./CommandClass";
+
+describe("lib/commandclass/BasicCC => ", () => {
+	it("the Get command should serialize correctly", () => {
+		const basicCC = new BasicCC(1, BasicCommand.Get);
+		const expected = Buffer.from([
+			1, // node number
+			2, // remaining length
+			CommandClasses.Basic, // CC
+			BasicCommand.Get, // CC Command
+		]);
+		basicCC.serialize().should.deep.equal(expected);
+	});
+
+	it("the Set command should serialize correctly", () => {
+		const basicCC = new BasicCC(2, BasicCommand.Set, 55);
+		const expected = Buffer.from([
+			2, // node number
+			3, // remaining length
+			CommandClasses.Basic, // CC
+			BasicCommand.Set, // CC Command
+			55, // target value
+		]);
+		basicCC.serialize().should.deep.equal(expected);
+	});
+
+	it("serialize() should throw for other commands", () => {
+		const basicCC = new BasicCC(2, -1 /* not a command */);
+		expect(() => basicCC.serialize())
+			.to.throw("Cannot serialize")
+			.and.be.an.instanceof(ZWaveError)
+			.and.satisfy((err: ZWaveError) => err.code === ZWaveErrorCodes.CC_Invalid)
+			;
+	});
+
+	it("the Report command (v1) should be deserialized correctly", () => {
+		const ccData = Buffer.from([
+			2, // node number
+			3, // remaining length
+			CommandClasses.Basic, // CC
+			BasicCommand.Report, // CC Command
+			55, // current value
+		]);
+		const basicCC = new BasicCC();
+		basicCC.deserialize(ccData);
+
+		basicCC.currentValue.should.equal(55);
+		expect(basicCC.targetValue).to.be.undefined;
+		expect(basicCC.duration).to.be.undefined;
+	});
+
+	it("the Report command (v1) should be deserialized correctly", () => {
+		const ccData = Buffer.from([
+			2, // node number
+			5, // remaining length
+			CommandClasses.Basic, // CC
+			BasicCommand.Report, // CC Command
+			55, // current value
+			66, // target value
+			1, // duration
+		]);
+		const basicCC = new BasicCC();
+		basicCC.deserialize(ccData);
+
+		basicCC.currentValue.should.equal(55);
+		basicCC.targetValue.should.equal(66);
+		basicCC.duration.should.equal(1);
+	});
+
+	it("deserialize() should throw for other commands", () => {
+		const serializedCC = Buffer.from([
+			2, // node number
+			2, // remaining length
+			CommandClasses.Basic, // CC
+			255, // not a valid command
+		]);
+		const basicCC = new BasicCC();
+		expect(() => basicCC.deserialize(serializedCC))
+			.to.throw("Cannot deserialize")
+			.and.be.an.instanceof(ZWaveError)
+			.and.satisfy((err: ZWaveError) => err.code === ZWaveErrorCodes.CC_Invalid)
+			;
+	});
+
+});
