d5bd83897f2439ff00696850686858fc7dc4ffce
Dominic Griesel <d.griesel@gmx.net>
2019-05-01 12:42:57 +0200

    Switch debug to TS-Node

--- test/run.ts
+++ test/run.ts
@@ -2,20 +2,27 @@
 require("reflect-metadata");
 
 import { wait } from "alcalzone-shared/async";
-import { BatteryCCGet } from "../src/lib/commandclass/BatteryCC";
 import { Driver } from "../src/lib/driver/Driver";
 
 const d = new Driver("COM3").once("driver ready", async () => {
-	const node = d.controller!.nodes.get(2)!;
-	node.on("value added", args =>
-		console.log(`value added: ${JSON.stringify(args)}`),
-	);
-	node.on("value updated", args =>
-		console.log(`value updated: ${JSON.stringify(args)}`),
-	);
-	node.on("value removed", args =>
-		console.log(`value removed: ${JSON.stringify(args)}`),
-	);
+	for (const nodeId of [2, 3]) {
+		const node = d.controller!.nodes.get(nodeId)!;
+		node.on("value added", args =>
+			console.log(
+				`[Node ${nodeId}] value added: ${JSON.stringify(args)}`,
+			),
+		);
+		node.on("value updated", args =>
+			console.log(
+				`[Node ${nodeId}] value updated: ${JSON.stringify(args)}`,
+			),
+		);
+		node.on("value removed", args =>
+			console.log(
+				`[Node ${nodeId}] value removed: ${JSON.stringify(args)}`,
+			),
+		);
+	}
 
 	// d.controller.nodes.get(2).on("value added", (args) => {
 	// 	console.log(`value added: cc=${args.commandClass}, propertyName=${args.propertyName} => value=${args.newValue}`);
@@ -33,19 +40,19 @@ const d = new Driver("COM3").once("driver ready", async () => {
 	// const resp = await d.sendMessage(new AddNodeToNetworkRequest(AddNodeType.Any, true, true));
 	// console.log(JSON.stringify(resp, null, 4));
 
-	node.on("interview completed", async () => {
-		const report = await d.sendCommand(new BatteryCCGet(d, { nodeId: 2 }));
-		console.log(JSON.stringify(report));
+	// node.on("interview completed", async () => {
+	// 	const report = await d.sendCommand(new BatteryCCGet(d, { nodeId: 2 }));
+	// 	console.log(JSON.stringify(report));
 
-		await wait(30000);
+	// 	await wait(30000);
 
-		await d.destroy();
-		process.exit(0);
-	});
+	// 	await d.destroy();
+	// 	process.exit(0);
+	// });
 
-	// await wait(60000);
-	// await d.destroy();
-	// process.exit(0);
+	await wait(600000);
+	await d.destroy();
+	process.exit(0);
 	// const resp = await d.sendMessage(new SetSerialApiTimeoutsRequest());
 });
 d.start();
