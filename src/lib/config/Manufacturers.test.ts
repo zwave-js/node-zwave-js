3c701e5387d54fc869eb3defca120e03af5b5943
dominic.griesel <d.griesel@gmx.net>
2019-05-02 16:16:02 +0200

    Add tests for config/Manufacturers

---
+++ src/lib/config/Manufacturers.test.ts
@@ -0,0 +1,34 @@
+import * as fsExtra from "fs-extra";
+import { lookupManufacturer } from "./Manufacturers";
+
+jest.mock("fs-extra");
+const readFileMock = fsExtra.readFile as jest.Mock;
+
+describe("lib/config/Manufacturers", () => {
+	describe("lookupManufacturer()", () => {
+		beforeEach(() => {
+			readFileMock.mockClear();
+		});
+
+		beforeAll(() => {
+			readFileMock.mockResolvedValue(
+				JSON.stringify({
+					"0x000e": "Test",
+				}),
+			);
+		});
+
+		it("caches the file contents", async () => {
+			await lookupManufacturer(0);
+			expect(fsExtra.readFile).toBeCalledTimes(1);
+
+			await lookupManufacturer(1);
+			expect(fsExtra.readFile).toBeCalledTimes(1);
+		});
+
+		it("returns the name belonging to the manufacturer ID if it is defined", async () => {
+			await expect(lookupManufacturer(0x0e)).resolves.toBe("Test");
+			await expect(lookupManufacturer(0xff)).resolves.toBeUndefined();
+		});
+	});
+});
