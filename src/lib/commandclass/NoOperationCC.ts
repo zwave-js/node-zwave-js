635d5ad9703c6dc44fa6fdfad3b0abee7fdd2d6a
dominic.griesel <d.griesel@gmx.net>
2019-07-09 10:33:13 +0200

    feat(cc): add API to NoOperationCC

--- src/lib/commandclass/NoOperationCC.ts
+++ src/lib/commandclass/NoOperationCC.ts
@@ -1,5 +1,30 @@
-import { CommandClass, commandClass, implementedVersion } from "./CommandClass";
+import { SendDataRequest } from "../controller/SendDataMessages";
+import { MessagePriority } from "../message/Constants";
+import { CCAPI } from "./API";
+import {
+	API,
+	CommandClass,
+	commandClass,
+	implementedVersion,
+} from "./CommandClass";
 import { CommandClasses } from "./CommandClasses";
+
+@API(CommandClasses["No Operation"])
+export class NoOperationCCAPI extends CCAPI {
+	public async send(): Promise<void> {
+		const request = new SendDataRequest(this.driver, {
+			command: new NoOperationCC(this.driver, {
+				nodeId: this.node.id,
+			}),
+		});
+		// Don't retry sending ping packets
+		request.maxSendAttempts = 1;
+		// set the priority manually, as SendData can be Application level too
+		await this.driver.sendMessage<SendDataRequest>(request, {
+			priority: MessagePriority.NodeQuery,
+		});
+	}
+}
 
 @commandClass(CommandClasses["No Operation"])
 @implementedVersion(1)
