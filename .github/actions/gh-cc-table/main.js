316304b39a6d745f93fdcfd9498e052fd0fc03a8
dominic.griesel <d.griesel@gmx.net>
2019-08-28 15:42:54 +0200

    le fix de bug

--- .github/actions/gh-cc-table/main.js
+++ .github/actions/gh-cc-table/main.js
@@ -1,42 +1,30 @@
 const c = require("ansi-colors");
-const exec = require('@actions/exec');
-const github = require('@actions/github');
-const core = require('@actions/core');
+const exec = require("@actions/exec");
+const github = require("@actions/github");
+const core = require("@actions/core");
 
-const githubToken = core.getInput('githubToken');
+const githubToken = core.getInput("githubToken");
 const octokit = new github.GitHub(githubToken);
 const context = github.context;
 
 (async function main() {
-
-	let ccTable = '';
+	let ccTable = "";
 
 	const options = {};
 	options.listeners = {
-		stdout: (data) => {
+		stdout: data => {
 			ccTable += data.toString();
 		},
 	};
 
-	await exec.exec('npm', ['run', 'gh-cc-table'], options);
+	await exec.exec("npm", ["run", "gh-cc-table"], options);
 
-	ccTable = ccTable.split("\n").filter(line => line.startsWith("| ")).join("\n");
+	ccTable = ccTable
+		.split("\n")
+		.filter(line => line.startsWith("| "))
+		.join("\n");
 	ccTable = c.stripColor(ccTable);
 
-	console.error()
-	console.error("FILTERED:");
-	console.error(ccTable);
-
-	const { data: { body } } = await octokit.issues.get({
-		...context.repo,
-		issue_number: 6,
-	});
-	console.error()
-	console.error("ISSUE BODY:");
-	console.error(body);
-
-	console.error()
-	console.error("NEW BODY:");
 	const newBody = `
 The following command classes are not implemented or incomplete:
 
@@ -47,6 +35,10 @@ ${ccTable}
 npm run gh-cc-table
 \`\`\`
 `;
-	console.error(newBody);
 
+	await octokit.issues.update({
+		...context.repo,
+		issue_number: 6,
+		body: newBody,
+	});
 })();
