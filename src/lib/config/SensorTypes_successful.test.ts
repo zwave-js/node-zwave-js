5c06dbd2b261b76f1a16248241af11f4d2c5ebd3
dominic.griesel <d.griesel@gmx.net>
2019-10-31 10:52:25 +0100

    test(config): add tests for sensor config

--- src/lib/config/SensorTypes_successful.test.ts
+++ src/lib/config/SensorTypes_successful.test.ts
@@ -1,112 +1,63 @@
-// import fsExtra from "fs-extra";
+import fsExtra from "fs-extra";
 
-// jest.mock("fs-extra");
-// const readFileMock = fsExtra.readFile as jest.Mock;
-// const pathExistsMock = fsExtra.pathExists as jest.Mock;
+jest.mock("fs-extra");
+const readFileMock = fsExtra.readFile as jest.Mock;
+const pathExistsMock = fsExtra.pathExists as jest.Mock;
 
-// const dummyNotifications = {
-// 	"0x0a": {
-// 		name: "Dummy Notification",
-// 		variables: [
-// 			{
-// 				name: "Some status",
-// 				states: {
-// 					"0x01": {
-// 						label: "Status 1",
-// 						description: "Some generic description",
-// 						version: 2,
-// 					},
-// 					"0x02": {
-// 						label: "Status 2",
-// 						version: 4,
-// 					},
-// 				},
-// 			},
-// 		],
-// 		events: {
-// 			"0x03": {
-// 				label: "An event",
-// 				version: 1,
-// 			},
-// 		},
-// 	},
-// };
+const dummySensorTypes = {
+	"0x0a": {
+		label: "Dummy temperature",
+		scales: {
+			"0x00": {
+				label: "Celcius",
+				unit: "°C",
+			},
+			"0x01": {
+				label: "Fahrenheit",
+				unit: "F",
+				description: "don't use this!",
+			},
+		},
+	},
+};
 
-// // I don't get why these tests don't play nice with the unsuccessful ones
-// // But since I see no other way, they are now in their own file
+// I don't get why these tests don't play nice with the unsuccessful ones
+// But since I see no other way, they are now in their own file
 
-// describe("lib/config/Notifications", () => {
-// 	describe("lookupNotification()", () => {
-// 		let lookupNotification: typeof import("./Notifications").lookupNotification;
+describe("lib/config/SensorTypes", () => {
+	describe("lookupSensorType()", () => {
+		let lookupSensorType: typeof import("./SensorTypes").lookupSensorType;
 
-// 		beforeAll(() => {
-// 			pathExistsMock.mockResolvedValue(true);
-// 			readFileMock.mockResolvedValue(JSON.stringify(dummyNotifications));
+		beforeAll(() => {
+			pathExistsMock.mockResolvedValue(true);
+			readFileMock.mockResolvedValue(JSON.stringify(dummySensorTypes));
 
-// 			jest.isolateModules(() => {
-// 				lookupNotification = require("./Notifications")
-// 					.lookupNotification;
-// 			});
-// 		});
+			jest.isolateModules(() => {
+				lookupSensorType = require("./SensorTypes").lookupSensorType;
+			});
+		});
 
-// 		beforeEach(() => {
-// 			readFileMock.mockClear();
-// 			pathExistsMock.mockClear();
-// 		});
+		beforeEach(() => {
+			readFileMock.mockClear();
+			pathExistsMock.mockClear();
+		});
 
-// 		it("caches the file contents", async () => {
-// 			await lookupNotification(0);
-// 			expect(fsExtra.readFile).toBeCalledTimes(1);
-// 			expect(fsExtra.pathExists).toBeCalledTimes(1);
+		it("caches the file contents", async () => {
+			await lookupSensorType(0);
+			expect(fsExtra.readFile).toBeCalledTimes(1);
+			expect(fsExtra.pathExists).toBeCalledTimes(1);
 
-// 			await lookupNotification(1);
-// 			expect(fsExtra.readFile).toBeCalledTimes(1);
-// 			expect(fsExtra.pathExists).toBeCalledTimes(1);
-// 		});
+			await lookupSensorType(1);
+			expect(fsExtra.readFile).toBeCalledTimes(1);
+			expect(fsExtra.pathExists).toBeCalledTimes(1);
+		});
 
-// 		it("returns the notification definition if it is defined", async () => {
-// 			const test1 = await lookupNotification(0x0a);
-// 			expect(test1).not.toBeUndefined();
-// 			expect(test1!.name).toBe("Dummy Notification");
+		it("returns the sensor type definition if it is defined", async () => {
+			const test1 = await lookupSensorType(0x0a);
+			expect(test1).not.toBeUndefined();
+			expect(test1!.label).toBe("Dummy temperature");
 
-// 			await expect(lookupNotification(0xff)).resolves.toBeUndefined();
-// 		});
-// 	});
-
-// 	describe("lookupValue()", () => {
-// 		let notification: import("./Notifications").Notification;
-// 		let lookupNotification: typeof import("./Notifications").lookupNotification;
-
-// 		beforeAll(() => {
-// 			pathExistsMock.mockClear();
-// 			readFileMock.mockClear();
-// 			pathExistsMock.mockResolvedValue(true);
-// 			readFileMock.mockResolvedValue(JSON.stringify(dummyNotifications));
-
-// 			jest.isolateModules(() => {
-// 				lookupNotification = require("./Notifications")
-// 					.lookupNotification;
-// 			});
-// 		});
-
-// 		beforeEach(async () => {
-// 			notification = (await lookupNotification(0x0a))!;
-// 			expect(notification).not.toBeUndefined();
-// 		});
-
-// 		it("returns undefined if nothing was defined with the given value", async () => {
-// 			const undefinedValue = await notification.lookupValue(0xffff);
-// 			expect(undefinedValue).toBeUndefined();
-// 		});
-
-// 		it("returns the type of the value and its definition otherwise", async () => {
-// 			const actual = await notification.lookupValue(0x01);
-// 			expect(actual).toMatchObject({
-// 				variableName: "Some status",
-// 				value: 1,
-// 				label: "Status 1",
-// 				description: "Some generic description",
-// 			});
-// 		});
-// 	});
-// });
+			await expect(lookupSensorType(0xff)).resolves.toBeUndefined();
+		});
+	});
+});
--- src/lib/config/SensorTypes_unsuccessful.test.ts
+++ src/lib/config/SensorTypes_unsuccessful.test.ts
@@ -1,73 +1,71 @@
-// import fsExtra from "fs-extra";
+import fsExtra from "fs-extra";
 
-// jest.mock("fs-extra");
-// const readFileMock = fsExtra.readFile as jest.Mock;
-// const pathExistsMock = fsExtra.pathExists as jest.Mock;
+jest.mock("fs-extra");
+const readFileMock = fsExtra.readFile as jest.Mock;
+const pathExistsMock = fsExtra.pathExists as jest.Mock;
 
-// describe("lib/config/Notifications", () => {
-// 	describe("lookupNotification (with missing file)", () => {
-// 		let lookupNotification: typeof import("./Notifications").lookupNotification;
+describe("lib/config/SensorTypes", () => {
+	describe("lookupSensorType (with missing file)", () => {
+		let lookupSensorType: typeof import("./SensorTypes").lookupSensorType;
 
-// 		beforeAll(() => {
-// 			pathExistsMock.mockClear();
-// 			readFileMock.mockClear();
-// 			pathExistsMock.mockResolvedValue(false);
-// 			readFileMock.mockRejectedValue(new Error("File does not exist"));
-// 			jest.isolateModules(() => {
-// 				lookupNotification = require("./Notifications")
-// 					.lookupNotification;
-// 			});
-// 		});
+		beforeAll(() => {
+			pathExistsMock.mockClear();
+			readFileMock.mockClear();
+			pathExistsMock.mockResolvedValue(false);
+			readFileMock.mockRejectedValue(new Error("File does not exist"));
+			jest.isolateModules(() => {
+				lookupSensorType = require("./SensorTypes").lookupSensorType;
+			});
+		});
 
-// 		it("does not throw", async () => {
-// 			await expect(lookupNotification(0)).not.toReject();
-// 		});
+		it("does not throw", async () => {
+			await expect(lookupSensorType(0)).not.toReject();
+		});
 
-// 		it("tries finding the file only once", async () => {
-// 			await lookupNotification(0);
-// 			expect(fsExtra.readFile).toBeCalledTimes(0);
-// 			expect(fsExtra.pathExists).toBeCalledTimes(1);
+		it("tries finding the file only once", async () => {
+			await lookupSensorType(0);
+			expect(fsExtra.readFile).toBeCalledTimes(0);
+			expect(fsExtra.pathExists).toBeCalledTimes(1);
 
-// 			await lookupNotification(1);
-// 			expect(fsExtra.readFile).toBeCalledTimes(0);
-// 			expect(fsExtra.pathExists).toBeCalledTimes(1);
-// 		});
+			await lookupSensorType(1);
+			expect(fsExtra.readFile).toBeCalledTimes(0);
+			expect(fsExtra.pathExists).toBeCalledTimes(1);
+		});
 
-// 		it("returns undefined", async () => {
-// 			await expect(lookupNotification(0x0e)).resolves.toBeUndefined();
-// 			await expect(lookupNotification(0xff)).resolves.toBeUndefined();
-// 		});
-// 	});
+		it("returns undefined", async () => {
+			await expect(lookupSensorType(0x0e)).resolves.toBeUndefined();
+			await expect(lookupSensorType(0xff)).resolves.toBeUndefined();
+		});
+	});
 
-// 	describe("lookupNotification (with invalid file)", () => {
-// 		let lookupNotification: typeof import("./Notifications").lookupNotification;
+	describe("lookupNotification (with invalid file)", () => {
+		let lookupSensorType: typeof import("./SensorTypes").lookupSensorType;
 
-// 		beforeAll(() => {
-// 			pathExistsMock.mockClear();
-// 			readFileMock.mockClear();
-// 			pathExistsMock.mockResolvedValue(true);
-// 			readFileMock.mockResolvedValue(`{"0x01": `);
+		beforeAll(() => {
+			pathExistsMock.mockClear();
+			readFileMock.mockClear();
+			pathExistsMock.mockResolvedValue(true);
+			readFileMock.mockResolvedValue(`{"0x01": `);
 
-// 			jest.isolateModules(() => {
-// 				lookupNotification = require("./Notifications")
-// 					.lookupNotification;
-// 			});
-// 		});
+			jest.isolateModules(() => {
+				lookupSensorType = require("./SensorTypes").lookupSensorType;
+			});
+		});
 
-// 		it("does not throw", async () => {
-// 			await expect(lookupNotification(0x0e)).not.toReject();
-// 		});
+		it("does not throw", async () => {
+			await expect(lookupSensorType(0x0e)).not.toReject();
+		});
 
-// 		it("tries loading the file only once", async () => {
-// 			await lookupNotification(0);
-// 			expect(fsExtra.readFile).toBeCalledTimes(1);
+		it("tries loading the file only once", async () => {
+			await lookupSensorType(0);
+			expect(fsExtra.readFile).toBeCalledTimes(1);
 
-// 			await lookupNotification(1);
-// 			expect(fsExtra.readFile).toBeCalledTimes(1);
-// 		});
+			await lookupSensorType(1);
+			expect(fsExtra.readFile).toBeCalledTimes(1);
+		});
 
-// 		it("returns undefined", async () => {
-// 			await expect(lookupNotification(0x0e)).resolves.toBeUndefined();
-// 		});
-// 	});
-// });
+		it("returns undefined", async () => {
+			await expect(lookupSensorType(0x0e)).resolves.toBeUndefined();
+		});
+	});
+});
