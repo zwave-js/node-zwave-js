29f6bb12a675414946896532a7c478b646418640
dominic.griesel <d.griesel@gmx.net>
2019-10-31 10:55:19 +0100

    chore(config): add lint rule for sensor config files

--- maintenance/lintConfigFiles.ts
+++ maintenance/lintConfigFiles.ts
@@ -6,6 +6,7 @@ import { pathExists, readFile } from "fs-extra";
 import JSON5 from "json5";
 import path from "path";
 import { Notification } from "../src/lib/config/Notifications";
+import { SensorType } from "../src/lib/config/SensorTypes";
 import { configDir } from "../src/lib/config/utils";
 
 const hexKeyRegex = /^0x[a-fA-F0-9]+$/;
@@ -68,9 +69,40 @@ async function lintManufacturers(): Promise<void> {
 	}
 }
 
+async function lintSensorTypes(): Promise<void> {
+	const configPath = path.join(configDir, "sensorTypes.json");
+	if (!(await pathExists(configPath))) {
+		throw new Error("The sensor types config file does not exist!");
+	}
+
+	const fileContents = await readFile(configPath, "utf8");
+	let definition: any;
+	try {
+		definition = JSON5.parse(fileContents);
+	} catch (e) {
+		throw new Error(`The sensor types config file is invalid: ${e}`);
+	}
+
+	if (!isObject(definition)) {
+		throw new Error("The sensor types config file must contain an object");
+	}
+
+	for (const [id, snsrDefinition] of entries(definition)) {
+		if (!hexKeyRegex.test(id)) {
+			throw new Error(
+				`The sensor types config file is invalid: found non-hex object key ${id}`,
+			);
+		}
+		const idNum = parseInt(id.slice(2), 16);
+		// TODO: Validate that all contents are semantically correct
+		const _testParse = new SensorType(idNum, snsrDefinition);
+	}
+}
+
 Promise.resolve()
-	.then(lintNotifications)
 	.then(lintManufacturers)
+	.then(lintNotifications)
+	.then(lintSensorTypes)
 	// TODO: lint device files
 	.then(() => {
 		console.error(green("The config files are valid!"));
