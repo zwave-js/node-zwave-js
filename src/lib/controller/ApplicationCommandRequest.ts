c15d70ec67cbb156c92c3f3cfdb9c0b478c4f0d7
dominic.griesel <d.griesel@gmx.net>
2020-02-19 17:16:55 +0100

    feat: parse more status flags

--- src/lib/controller/ApplicationCommandRequest.ts
+++ src/lib/controller/ApplicationCommandRequest.ts
@@ -16,14 +16,24 @@ import {
 } from "../message/Message";
 
 enum StatusFlags {
-	RoutedBusy = 1 << 0,
-	Broadcast = 1 << 2,
+	RoutedBusy = 0b1, // A response route is locked by the application
+	LowPower = 0b10, // Received at low output power level
+
+	TypeSingle = 0b0000, // Received a single cast frame
+	TypeBroad = 0b0100, // Received a broad cast frame
+	TypeMulti = 0b1000, // Received a multi cast frame
+	TypeMask = TypeSingle | TypeBroad | TypeMulti,
+
+	Explore = 0b10000, // Received an explore frame
+
+	ForeignFrame = 0b0100_0000, // Received a foreign frame (only promiscous mode)
+	ForeignHomeId = 0b1000_0000, // The received frame is received from a foreign HomeID. Only Controllers in Smart Start AddNode mode can receive this status.
 }
 
 interface ApplicationCommandRequestOptions extends MessageBaseOptions {
-	isBroadcast?: boolean;
-	routedBusy?: boolean;
 	command: CommandClass;
+	frameType?: ApplicationCommandRequest["frameType"];
+	routedBusy?: boolean;
 }
 
 @messageTypes(MessageType.Request, FunctionType.ApplicationCommand)
@@ -41,8 +51,23 @@ export class ApplicationCommandRequest extends Message
 		if (gotDeserializationOptions(options)) {
 			// first byte is a status flag
 			const status = this.payload[0];
-			this._isBroadcast = (status & StatusFlags.Broadcast) !== 0;
-			this._routedBusy = (status & StatusFlags.RoutedBusy) !== 0;
+			this.routedBusy = !!(status & StatusFlags.RoutedBusy);
+			switch (status & StatusFlags.TypeMask) {
+				case StatusFlags.TypeMulti:
+					this.frameType = "multicast";
+					break;
+				case StatusFlags.TypeBroad:
+					this.frameType = "broadcast";
+					break;
+				default:
+					this.frameType = "singlecast";
+			}
+			this.isExploreFrame =
+				this.frameType === "broadcast" &&
+				!!(status & StatusFlags.Explore);
+			this.isForeignFrame = !!(status & StatusFlags.ForeignFrame);
+			this.fromForeignHomeId = !!(status & StatusFlags.ForeignHomeId);
+
 			// followed by a node ID
 			const nodeId = this.payload[1];
 			// and a command class
@@ -52,29 +77,31 @@ export class ApplicationCommandRequest extends Message
 				nodeId,
 			});
 		} else {
-			this._isBroadcast = !!options.isBroadcast;
-			this._routedBusy = !!options.routedBusy;
+			this.frameType = options.frameType ?? "singlecast";
+			this.routedBusy = !!options.routedBusy;
 			this.command = options.command;
+			this.isExploreFrame = false;
+			this.isForeignFrame = false;
+			this.fromForeignHomeId = false;
 		}
 	}
 
-	private _routedBusy: boolean;
-	public get routedBusy(): boolean {
-		return this._routedBusy;
-	}
-
-	private _isBroadcast: boolean;
-	public get isBroadcast(): boolean {
-		return this._isBroadcast;
-	}
+	public readonly routedBusy: boolean;
+	public readonly frameType: "singlecast" | "broadcast" | "multicast";
+	public readonly isExploreFrame: boolean;
+	public readonly isForeignFrame: boolean;
+	public readonly fromForeignHomeId: boolean;
 
 	// This needs to be writable or unwrapping MultiChannelCCs crashes
 	public command: CommandClass;
 
 	public serialize(): Buffer {
 		const statusByte =
-			(this._isBroadcast ? StatusFlags.Broadcast : 0) |
-			(this._routedBusy ? StatusFlags.RoutedBusy : 0);
+			(this.frameType === "broadcast"
+				? StatusFlags.TypeBroad
+				: this.frameType === "multicast"
+				? StatusFlags.TypeMulti
+				: 0) | (this.routedBusy ? StatusFlags.RoutedBusy : 0);
 
 		const serializedCC = this.command.serialize();
 		this.payload = Buffer.concat([
