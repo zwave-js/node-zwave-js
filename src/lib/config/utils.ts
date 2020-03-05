b5696031ebf70f9331107926e92d99f9182b7915
dominic.griesel <d.griesel@gmx.net>
2020-03-05 16:01:14 +0100

    feat: flag duplicate config files as errors

--- src/lib/config/utils.ts
+++ src/lib/config/utils.ts
@@ -1,5 +1,8 @@
+import { padStart } from "alcalzone-shared/strings";
 import path from "path";
+import * as semver from "semver";
 import { ZWaveError, ZWaveErrorCodes } from "../error/ZWaveError";
+import { DeviceConfigIndexEntry } from "./Devices";
 
 export const configDir = path.resolve(__dirname, "../../..", "config");
 export const hexKeyRegexNDigits = /^0x[a-fA-F0-9]+$/;
@@ -13,4 +16,34 @@ export function throwInvalidConfig(which: string, reason?: string): never {
 			: "",
 		ZWaveErrorCodes.Config_Invalid,
 	);
+}
+
+export function getDeviceEntryPredicate(
+	manufacturerId: number,
+	productType: number,
+	productId: number,
+	firmwareVersion?: string,
+): (entry: DeviceConfigIndexEntry) => boolean {
+	return entry =>
+		entry.manufacturerId === formatId(manufacturerId) &&
+		entry.productType === formatId(productType) &&
+		entry.productId === formatId(productId) &&
+		(firmwareVersion == undefined ||
+			(semver.lte(
+				padVersion(entry.firmwareVersion.min),
+				padVersion(firmwareVersion),
+			) &&
+				semver.gte(
+					padVersion(entry.firmwareVersion.max),
+					padVersion(firmwareVersion),
+				)));
+}
+
+export function formatId(id: number): string {
+	return "0x" + padStart(id.toString(16), 4, "0");
+}
+
+/** Pads a firmware version string, so it can be compared with semver */
+export function padVersion(version: string): string {
+	return version + ".0";
 }
