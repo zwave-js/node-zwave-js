6cb3ef080204e715a6eb79185915c5530ee767d2
AlCalzone <d.griesel@gmx.net>
2020-04-14 14:39:12 +0200

    Support ColorSwitchCC (#766)
    * First pass of Color Switch Report.  Command IDs may be incorrect.
    
    * Fix loading command classes.
    
    * Decorator metadata is unused and is causing crashes on load.  Disable it.
    
    * Fix folder name.
    
    * Convert import to type import
    
    * Convert import to type import
    
    * Implement ColorSwitch v2 duration.
    
    * Fix index not sending with ColorSwitchCCSet
    
    * Fix build: ColorSwitchCCSet._duration must have an initial value.
    
    * Feedback
    
    * Ensure ColorSwitchCCGet retreives the correct report.
    
    * Tests and fixes to ColorSwitchCC.
    
    * ColorSwitch StartLevelChange/StopLevelChange
    
    * ColorSwitch v3.  Handle durations.
    
    * ColorSwitch interview process.
    
    * Fix ignoreStartLevel in ColorSwitch StartLevelChange
    
    * lint
    
    * Add missing commands to supportsCommand.
    
    * Add additional props to ColorSwitchAPI.get return value.
    
    * Use ccValues and propertyKey for supportedColor, currentColor, targetColor
    
    * Use color keys instead of ordinals.
    
    * Properties of reports are undefined when report has not executed.
    
    * Fix build
    
    * Add startLevelChange / stopLevelChange to ColorSwitch API.
    
    * Feedback
    
    * Util classes for valueID creation.  Move metadata creation to interview.
    
    * chore(todo): remove outdated todos
    
    * chore: add JSDoc comments
    
    * fix: use ZWaveErrors instead of plain Errors
    
    * fix: only pick key if it exists in the source object
    
    * refactor: change API and ValueID structure to match other CCs
    
    * feat: expose duration in API.set
    
    * test: fix tests
    
    * feat: translate numeric property key to color component string
    
    Co-authored-by: RoboPhred <robophred@gmail.com>

---
+++ src/lib/commandclass/ColorSwitchCC.test.ts
@@ -0,0 +1,272 @@
+import { createEmptyMockDriver } from "../../../test/mocks";
+import type { Driver } from "../driver/Driver";
+import { ZWaveNode } from "../node/Node";
+import { Duration } from "../values/Duration";
+import {
+	ColorComponent,
+	ColorSwitchCCGet,
+	ColorSwitchCCReport,
+	ColorSwitchCCSet,
+	ColorSwitchCCStartLevelChange,
+	ColorSwitchCCStopLevelChange,
+	ColorSwitchCCSupportedGet,
+	ColorSwitchCCSupportedReport,
+	ColorSwitchCommand,
+} from "./ColorSwitchCC";
+import { CommandClasses } from "./CommandClasses";
+
+const fakeDriver = (createEmptyMockDriver() as unknown) as Driver;
+const node1 = new ZWaveNode(1, fakeDriver as any);
+(fakeDriver.controller!.nodes as any).set(1, node1);
+
+function buildCCBuffer(payload: Buffer): Buffer {
+	return Buffer.concat([
+		Buffer.from([
+			CommandClasses["Color Switch"], // CC
+		]),
+		payload,
+	]);
+}
+
+describe.only("lib/commandclass/ColorSwitchCC => ", () => {
+	it("the SupportedGet command should serialize correctly", () => {
+		const cc = new ColorSwitchCCSupportedGet(fakeDriver, {
+			nodeId: 1,
+		});
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.SupportedGet, // CC Command
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the SupportedReport command should deserialize correctly", () => {
+		const ccData = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.SupportedReport, // CC Command
+				0b0001_1111,
+				0b0000_0001,
+			]),
+		);
+		const cc = new ColorSwitchCCSupportedReport(fakeDriver, {
+			nodeId: 1,
+			data: ccData,
+		});
+
+		expect(cc.supportedColorComponents).toIncludeAllMembers([
+			ColorComponent["Cold White"],
+			ColorComponent["Warm White"],
+			ColorComponent.Red,
+			ColorComponent.Green,
+			ColorComponent.Blue,
+			ColorComponent.Index,
+		]);
+		expect(cc.supportedColorComponents).not.toIncludeAnyMembers([
+			ColorComponent.Amber,
+			ColorComponent.Cyan,
+			ColorComponent.Purple,
+		]);
+	});
+
+	it("the Get command should serialize correctly", () => {
+		const cc = new ColorSwitchCCGet(fakeDriver, {
+			nodeId: 1,
+			colorComponent: ColorComponent.Red,
+		});
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.Get, // CC Command
+				2, // Color Component
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the Report command should deserialize correctly (version 1)", () => {
+		const ccData = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.Report, // CC Command
+				0b0000_0010, // color: red
+				0b1111_1111, // value: 255
+			]),
+		);
+		const cc = new ColorSwitchCCReport(fakeDriver, {
+			nodeId: 1,
+			data: ccData,
+		});
+
+		expect(cc.colorComponent).toBe(ColorComponent.Red);
+		expect(cc.currentValue).toBe(255);
+		expect(cc.targetValue).toBeUndefined();
+		expect(cc.duration).toBeUndefined();
+	});
+
+	it("the Report command should deserialize correctly (version 3)", () => {
+		const ccData = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.Report, // CC Command
+				0b0000_0010, // color: red
+				0b1000_0000, // currentValue: 128
+				0b1111_1111, // targetValue: 255,
+				0b0000_0001, // duration: 1
+			]),
+		);
+		const cc = new ColorSwitchCCReport(fakeDriver, {
+			nodeId: 1,
+			data: ccData,
+		});
+
+		expect(cc.colorComponent).toBe(ColorComponent.Red);
+		expect(cc.currentValue).toBe(128);
+		expect(cc.targetValue).toBe(255);
+		expect(cc.duration!.value).toBe(1);
+		expect(cc.duration!.unit).toBe("seconds");
+	});
+
+	it("the Set command should serialize correctly (version 1)", () => {
+		const cc = new ColorSwitchCCSet(fakeDriver, {
+			nodeId: 1,
+			red: 128,
+			green: 255,
+		});
+		cc.version = 1;
+
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.Set, // CC Command
+				// WARN: This is sensitive to the order in which javascript serializes the colorTable keys.
+				0b0000_0010, // reserved + count
+				0b0000_0010, // color: red
+				0b1000_0000, // value: 128
+				0b0000_0011, // color: green
+				0b1111_1111, // value: 255
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the Set command should serialize correctly (version 2)", () => {
+		const cc = new ColorSwitchCCSet(fakeDriver, {
+			nodeId: 1,
+			red: 128,
+			green: 255,
+			duration: new Duration(1, "seconds"),
+		});
+		cc.version = 2;
+
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.Set, // CC Command
+				// WARN: This is sensitive to the order in which javascript serializes the colorTable keys.
+				0b0000_0010, // reserved + count
+				0b0000_0010, // color: red
+				0b1000_0000, // value: 128
+				0b0000_0011, // color: green
+				0b1111_1111, // value: 255
+				0b0000_0001, // duration: 1
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the StartLevelChange command should serialize correctly (up) (version 1)", () => {
+		const cc = new ColorSwitchCCStartLevelChange(fakeDriver, {
+			nodeId: 1,
+			ignoreStartLevel: false,
+			startLevel: 5,
+			direction: "up",
+			colorComponent: ColorComponent.Red,
+		});
+		cc.version = 1;
+
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.StartLevelChange,
+				0b0000_0000, // up/down: 0, ignoreStartLevel: 0
+				0b0000_0010, // color: red
+				0b0000_0101, // startLevel: 5
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the StartLevelChange command should serialize correctly (down) (version 1)", () => {
+		const cc = new ColorSwitchCCStartLevelChange(fakeDriver, {
+			nodeId: 1,
+			startLevel: 5,
+			ignoreStartLevel: false,
+			direction: "down",
+			colorComponent: ColorComponent.Red,
+		});
+		cc.version = 1;
+
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.StartLevelChange,
+				0b0100_0000, // up/down: 0, ignoreStartLevel: 0
+				0b0000_0010, // color: red
+				0b0000_0101, // startLevel: 5
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the StartLevelChange command should serialize correctly (ignoreStartLevel) (version 1)", () => {
+		const cc = new ColorSwitchCCStartLevelChange(fakeDriver, {
+			nodeId: 1,
+			colorComponent: ColorComponent.Red,
+			ignoreStartLevel: true,
+			direction: "up",
+		});
+		cc.version = 1;
+
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.StartLevelChange,
+				0b0010_0000, // up/down: 0, ignoreStartLevel: 1
+				0b0000_0010, // color: red
+				0b0000_0000, // startLevel: 0
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the StartLevelChange command should serialize correctly (duration) (version 3)", () => {
+		const cc = new ColorSwitchCCStartLevelChange(fakeDriver, {
+			nodeId: 1,
+			startLevel: 5,
+			ignoreStartLevel: false,
+			direction: "up",
+			colorComponent: ColorComponent.Red,
+			duration: new Duration(1, "seconds"),
+		});
+		cc.version = 3;
+
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.StartLevelChange,
+				0b0000_0000, // up/down: 0, ignoreStartLevel: 0
+				0b0000_0010, // color: red
+				0b0000_0101, // startLevel: 5
+				0b0000_0001, // duration: 1
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+
+	it("the StopLevelChange command should serialize correctly", () => {
+		const cc = new ColorSwitchCCStopLevelChange(fakeDriver, {
+			nodeId: 1,
+			colorComponent: ColorComponent.Red,
+		});
+
+		const expected = buildCCBuffer(
+			Buffer.from([
+				ColorSwitchCommand.StopLevelChange, // CC Command
+				0b0000_0010, // color: red
+			]),
+		);
+		expect(cc.serialize()).toEqual(expected);
+	});
+});
