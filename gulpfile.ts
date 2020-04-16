90cef4372f0bef1f08769c99f7de9003ddfcb18c
Dominic Griesel <d.griesel@gmx.net>
2020-04-16 16:01:28 +0200

    build: clean old artifacts before building

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,13 +1,19 @@
-import { parallel } from "gulp";
+import { parallel, series } from "gulp";
 import { generateCCAPIInterface } from "./maintenance/generateCCAPIInterface";
 import { generateCCExports } from "./maintenance/generateCCExports";
-import { copyIndexFilesToRoot } from "./maintenance/packageStructure";
+import { clean, copyIndexFilesToRoot } from "./maintenance/packageStructure";
 
 export { lintConfigFiles } from "./maintenance/lintConfigFiles";
-export { clean } from "./maintenance/packageStructure";
-export { generateCCAPIInterface, generateCCExports };
-export { copyIndexFilesToRoot /*, restorePackageStructure*/ };
+export {
+	generateCCAPIInterface,
+	generateCCExports,
+	clean,
+	copyIndexFilesToRoot,
+};
 
-export const prebuild = parallel(generateCCAPIInterface, generateCCExports);
+export const prebuild = series(
+	clean,
+	parallel(generateCCAPIInterface, generateCCExports),
+);
 export const postbuild = copyIndexFilesToRoot;
 // export const postpack = restorePackageStructure;
