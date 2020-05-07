655f2113269d3b533fd8aa7a09077d3a41169874
Dominic Griesel <d.griesel@gmx.net>
2020-05-07 13:10:48 +0200

    build: check that no non-application CC depends on application CCs

---
+++ maintenance/shared.ts
@@ -0,0 +1,53 @@
+import * as path from "path";
+import * as prettier from "prettier";
+import ts from "typescript";
+
+// Find this project's root dir
+export const projectRoot = path.join(__dirname, "..");
+
+export function loadTSConfig(): {
+	options: ts.CompilerOptions;
+	fileNames: string[];
+} {
+	const configFileName = ts.findConfigFile(
+		"../",
+		ts.sys.fileExists,
+		"tsconfig.json",
+	);
+	if (!configFileName) throw new Error("tsconfig.json not found");
+
+	const configFileText = ts.sys.readFile(configFileName);
+	if (!configFileText) throw new Error("could not read tsconfig.json");
+
+	const parsedCommandLine = ts.getParsedCommandLineOfConfigFile(
+		configFileName,
+		{},
+		ts.sys as any,
+	);
+	if (!parsedCommandLine) throw new Error("could not parse tsconfig.json");
+
+	return {
+		options: parsedCommandLine.options,
+		fileNames: parsedCommandLine.fileNames,
+	};
+}
+
+export function compareStrings(a: string, b: string): number {
+	if (a > b) return 1;
+	if (b > a) return -1;
+	return 0;
+}
+
+// Make the linter happy
+export function formatWithPrettier(
+	filename: string,
+	sourceText: string,
+): string {
+	// Keep this in sync with templates/_prettierrc.js.ts
+	const prettierOptions = {
+		...require("../.prettierrc"),
+		// To infer the correct parser
+		filepath: filename,
+	};
+	return prettier.format(sourceText, prettierOptions);
+}
