2dfa4722512eff129dc6062e5fe9bc607235b60a
Dominic Griesel <d.griesel@gmx.net>
2020-06-02 10:52:28 +0200

    fix(build): generate source maps again

--- maintenance/compile.ts
+++ maintenance/compile.ts
@@ -1,16 +1,23 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import gulp from "gulp";
+import sourcemaps from "gulp-sourcemaps";
 import ts from "gulp-typescript";
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function compile(settings?: ts.Settings) {
 	const tsProject = ts.createProject("tsconfig.build.json", settings);
 	return tsProject
 		.src()
+		.pipe(sourcemaps.init())
 		.pipe(tsProject())
+		.pipe(
+			sourcemaps.write(".", {
+				includeContent: false,
+				sourceRoot: "../src",
+			}),
+		)
 		.pipe(gulp.dest(tsProject.options.outDir!));
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function check() {
 	return compile({ noEmit: true });
 }
