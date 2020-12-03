fa42f0703cb425291f050631c5cdab69efb90a5b
AlCalzone <d.griesel@gmx.net>
2020-12-03 16:08:07 +0100

    feat(docs): auto-import interfaces, types, enum declarations in the docs from TS sources (#1148)

--- packages/zwave-js/src/Driver.ts
+++ packages/zwave-js/src/Driver.ts
@@ -1,3 +1,4 @@
 export { Driver } from "./lib/driver/Driver";
-export type { ZWaveOptions } from "./lib/driver/Driver";
+export type { SendMessageOptions, ZWaveOptions } from "./lib/driver/Driver";
 export type { FileSystem } from "./lib/driver/FileSystem";
+export { MessagePriority } from "./lib/message/Constants";
