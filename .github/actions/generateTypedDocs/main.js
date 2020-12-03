c98d14dc771bee3231b915305498e1dce67ccfec
Dominic Griesel <d.griesel@gmx.net>
2020-12-03 23:36:34 +0100

    test(ci): debug PR creation

--- .github/actions/generateTypedDocs/main.js
+++ .github/actions/generateTypedDocs/main.js
@@ -42,9 +42,12 @@ const octokit = github.getOctokit(githubToken);
 		state: "open",
 		head: branchName,
 	});
+	console.dir(PRs);
 	const firstPR = PRs.data[0];
 	const currentId = firstPR && firstPR.id;
+	console.log(`current PR ID is ${currentId}`);
 	if (!currentId) {
+		console.log(`creating new PR...`);
 		// no PR exists, create one
 		await octokit.pulls.create({
 			owner: "zwave-js",
