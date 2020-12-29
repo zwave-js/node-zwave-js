217383ef93a3cf8a34eab87c80adbc5c479ae3b9
Daniel Lando <daniel.sorridi@gmail.com>
2020-12-29 10:39:32 +0100

    fix: export DeviceClass

--- packages/zwave-js/src/Node.ts
+++ packages/zwave-js/src/Node.ts
@@ -1,6 +1,7 @@
 export { NODE_ID_BROADCAST, NODE_ID_MAX } from "@zwave-js/core";
 export { Endpoint } from "./lib/node/Endpoint";
 export { ZWaveNode } from "./lib/node/Node";
+export { DeviceClass } from "./lib/node/DeviceClass";
 export {
 	InterviewStage,
 	NodeInterviewFailedEventArgs,
