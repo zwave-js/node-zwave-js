7f7d4c7a8ca17d714a89fbb2e4f2ba83666a4fad
Daniel Lando <daniel.sorridi@gmail.com>
2020-12-29 10:58:25 +0100

    fix: lint issues part 2

--- packages/zwave-js/src/Node.ts
+++ packages/zwave-js/src/Node.ts
@@ -1,7 +1,7 @@
 export { NODE_ID_BROADCAST, NODE_ID_MAX } from "@zwave-js/core";
+export { DeviceClass } from "./lib/node/DeviceClass";
 export { Endpoint } from "./lib/node/Endpoint";
 export { ZWaveNode } from "./lib/node/Node";
-export { DeviceClass } from "./lib/node/DeviceClass";
 export {
 	InterviewStage,
 	NodeInterviewFailedEventArgs,
