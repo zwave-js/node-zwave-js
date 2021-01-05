0a4a4302e14af1d712e4c7e18e4ec555ffd62b1e
AlCalzone <d.griesel@gmx.net>
2021-01-05 01:23:43 +0100

    chore: speed up CI tests (#1292)

--- packages/zwave-js/gulpfile.ts
+++ packages/zwave-js/gulpfile.ts
@@ -6,11 +6,11 @@ import { lintCCConstructors } from "./maintenance/lintCCConstructor";
 import { lintCCInterview } from "./maintenance/lintCCInterview";
 import { clean, copyIndexFilesToRoot } from "./maintenance/packageStructure";
 
-const prebuild = series(
-	parallel(lintCCInterview, lintCCConstructors),
-	parallel(generateCCAPIInterface, generateCCExports),
-);
+const lint = parallel(lintCCInterview, lintCCConstructors);
+const prebuild = parallel(generateCCAPIInterface, generateCCExports);
 
+// The gulp build task is used to create a "full build" - removing old build artifacts and ensuring the correct directory structure
+// For a quick test build, use the "tsc" way - until https://github.com/ivogabe/gulp-typescript/issues/611 is fixed
 const build = series(clean, prebuild, compile, copyIndexFilesToRoot);
 
-export { clean, prebuild, build, lintCCConstructors, lintCCInterview };
+export { clean, prebuild, build, lintCCConstructors, lintCCInterview, lint };
