2365b5fc206006f8512fae293209e0a9c5341bba
Dominic Griesel <d.griesel@gmx.net>
2021-01-05 17:05:43 +0100

    Revert "test: introduce lint error"
    This reverts commit 34b2729cd4166b9b25ce3d3dce5d65c18bbb2bd1.

--- packages/zwave-js/src/Driver.ts
+++ packages/zwave-js/src/Driver.ts
@@ -1,4 +1,4 @@
 export { Driver } from "./lib/driver/Driver";
 export type { SendMessageOptions, ZWaveOptions } from "./lib/driver/Driver";
-export type { FileSystem } from "./lib/driver/FileSystem"; 
+export type { FileSystem } from "./lib/driver/FileSystem";
 export { MessagePriority } from "./lib/message/Constants";
