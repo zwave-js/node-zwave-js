ae1137bd4b80432abca3b79dde368c7cc8cdb879
Dominic Griesel <d.griesel@gmx.net>
2021-01-08 00:10:35 +0100

    feat(bot): offer help when index file needs updating

--- .github/bot-scripts/index.js
+++ .github/bot-scripts/index.js
@@ -2,4 +2,8 @@ module.exports = {
 	fixLintOffer: (...args) => require("./fix-lint-offer")(...args),
 	fixLintCheck: (...args) => require("./fix-lint-check")(...args),
 	fixLintFeedback: (...args) => require("./fix-lint-feedback")(...args),
+	updateIndexOffer: (...args) => require("./update-index-offer")(...args),
+	updateIndexCheck: (...args) => require("./update-index-check")(...args),
+	updateIndexFeedback: (...args) =>
+		require("./update-index-feedback")(...args),
 };
