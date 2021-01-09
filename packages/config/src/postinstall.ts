f92f516e3e38db030360bb51bbd6a8b00675ae8c
Dominic Griesel <d.griesel@gmx.net>
2021-01-09 14:54:42 +0100

    fix: drop postinstall script

--- packages/config/src/postinstall.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigManager } from "./ConfigManager";
-
-(async () => {
-	const configManager = new ConfigManager();
-	console.log("generating device configuration index...");
-	await configManager.generateDeviceIndex();
-})().catch(() => process.exit(1));
