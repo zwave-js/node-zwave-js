aa5a65f01db6d41da27f28f97cf55e7a818b56a3
V. Aret <vassilis@aretakis.eu>
2021-01-10 15:27:01 +0200

    feat: barrier operator
    This commit adds:
    - Signaling Capabilities Get and Report

--- packages/zwave-js/src/lib/commandclass/BarrierOperatorCC.ts
+++ packages/zwave-js/src/lib/commandclass/BarrierOperatorCC.ts
@@ -7,14 +7,22 @@ import {
 	implementedVersion,
 	CommandClassDeserializationOptions,
 	gotDeserializationOptions,
+	ccValue,
 } from "./CommandClass";
-import { CommandClasses } from "@zwave-js/core";
+import { CommandClasses, parseBitMask, validatePayload } from "@zwave-js/core";
 import type { Driver } from "../driver/Driver";
+import { expectedResponse } from "../message/Message";
 
 // All the supported commands
 export enum BarrierOperatorCommand {
-	Set = 0x01
-	Get = 0x02
+	Set = 0x01,
+	Get = 0x02,
+	Report = 0x03,
+	CapabilitiesGet = 0x04,
+	CapabilitiesReport = 0x05,
+	EventSignalSet = 0x06,
+	EventSignalGet = 0x07,
+	EventSignalReport = 0x08,
 }
 
 
@@ -25,7 +33,6 @@ export enum BarrierReportState {
 	Stopped = 0xFD,
 	Openning = 0xFE,
 	Open = 0xFF
-
 }
 
 /**
@@ -37,6 +44,13 @@ export enum BarrierPosition {
 	Stopped = 0xFD
 	Openning = 0xFE,
 	Open = 0xFF,
+}
+
+// @publicAPI
+export enum SignalType {
+	"Not Supported" = 0x00,
+	Audible = 0x01,
+	Visual = 0x02,
 }
 
 @commandClass(CommandClasses.BarrierOperator)
@@ -112,3 +126,32 @@ export class BarrierOperatorCCReport extends BarrierOperatorCC {
 		return this._barrierPosition;
 	}
 }
+
+@CCCommand(BarrierOperatorCommand.CapabilitiesGet)
+@expectedResponse(BarrierOperatorCCCapabilitiesReport)
+export class BarrierOperatorCCCapabilitiesGet extends BarrierOperatorCC {}
+
+@CCCommand(BarrierOperatorCommand.CapabilitiesReport)
+export class BarrierOperatorCCCapabilitiesReport extends BarrierOperatorCC {
+	public constructor(
+		driver: Driver,
+		options: CommandClassDeserializationOptions,
+	) {
+		super(driver, options);
+		
+		validatePayload(this.payload.length >=1);
+		const bitMaskLength = this.payload.length;
+		validatePayload(this.payload.length >= bitMaskLength);
+		this._supportedSignalTypes = parseBitMask(
+			this.payload.slice(0, bitMaskLength),
+			SignalType['Not Supported'],
+		);
+
+		this this.persistValues();
+	}
+
+	private _supportedSignalTypes: SignalType[];
+	@@ccValue({ internal: true})
+	public get supportedSignalTypes(): readonly SignalType[] {
+		return this._supportedSignalTypes;
+	}
