c4e1de2d492c9706a44d929a330db79c4598ef7a
V. Aret <vassilis@aretakis.eu>
2021-01-10 14:38:19 +0200

    feat: barrier operator cc
    Adding first commit
    
    - init CC file
    - Add basic first commands (set/get/report)
    - Implement Set and Report command

--- packages/zwave-js/src/lib/commandclass/BarrierOperatorCC.ts
+++ packages/zwave-js/src/lib/commandclass/BarrierOperatorCC.ts
@@ -17,6 +17,28 @@ export enum BarrierOperatorCommand {
 	Get = 0x02
 }
 
+
+// All possible positions for Report
+export enum BarrierReportState {
+	Closed = 0x00,
+	Closing = 0xFC,
+	Stopped = 0xFD,
+	Openning = 0xFE,
+	Open = 0xFF
+
+}
+
+/**
+ * @publicAPI
+ */
+export enum BarrierPosition {
+	Closed = 0x00,
+	Closing = 0xFC,
+	Stopped = 0xFD
+	Openning = 0xFE,
+	Open = 0xFF,
+}
+
 @commandClass(CommandClasses.BarrierOperator)
 @implementedVersion(1)
 export class BarrierOperatorCC extends CommandClass {
@@ -54,6 +76,7 @@ export class BarrierOperatorCCSet extends BarrierOperatorCC {
 	}
 }
 
+/** Returns the State  */
 interface BarrierOperatorCCGetOptions extends CCCommandOptions {
 	someProperty: number;
 }
@@ -71,21 +94,21 @@ export class BarrierOperatorCCReport extends BarrierOperatorCC {
 		super(driver, options);
 
 		validatePayload(this.payload.length >= 1);
-		this._payload = this.payload[0];
-		if (this._payload === 0x00) {
-			this._closed
-		} else if (this._payload >= 0x01 & this._payload <= 0x63) {
-			this._stoppedPosition = this._payload
-		} else if (this._payload === 0xfc) {
-			this._closing
-		} else if (this._payload === 0xfd) {
-			this._stopped
-		} else if (this._payload === 0xfe) {
-			this._openning
-		} else if (this._payload === 0xff) {
-			this._open
-		}
-
+		
+		// return raw value if cannot decode
+		this._barrierPosition = this.payload[0];
 		this.persistValues();
+	}
+
+	private _barrierPosition: BarrierPosition | undefined;
+	@ccValue()
+	@ccValueMetadata({
+		...ValueMetadata.ReadOnlyUInt8,
+		label: "Barrier Operator State",
+		states: enumValuesToMetadataStates(BarrierPosition)
+	})
+
+	public get barrierPosition(): BarrierPosition | undefined {
+		return this._barrierPosition;
 	}
 }
