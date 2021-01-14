5ca3d213eb3c5624652d8c7be002ea9d67f8fa5b
Dominic Griesel <d.griesel@gmx.net>
2021-01-14 17:37:17 +0100

    fix(bot): request both reviewers on automatic doc updates

--- .github/actions/generateTypedDocs/main.js
+++ .github/actions/generateTypedDocs/main.js
@@ -10,19 +10,21 @@ const options = {
 	repo: "node-zwave-js",
 };
 const branchName = "docs/update-typed-docs";
-const reviewers = [
-	"robertsLando",
-];
+const reviewers = ["AlCalzone", "robertsLando"];
 const assignees = [
-	"AlCalzone",
+	// "AlCalzone",
+	// "robertsLando",
 ];
 
 (async function main() {
-
 	// check if our local working copy has any changes in the docs directory
-	const isChanged = !(await exec.exec("git", ["diff", "--exit-code", "--", "docs/"], {
-		ignoreReturnCode: true
-	}));
+	const isChanged = !(await exec.exec(
+		"git",
+		["diff", "--exit-code", "--", "docs/"],
+		{
+			ignoreReturnCode: true,
+		},
+	));
 	if (isChanged) {
 		console.log(`We have no local changes, exiting...`);
 		return;
@@ -36,22 +38,32 @@ const assignees = [
 	});
 	const firstPR = PRs.data[0];
 	let prNumber = firstPR && firstPR.number;
-	
+
 	// Check if the action's branch exists on the remote (exit code 0) or not (exit code 2)
-	const branchExists = !(await exec.exec("git", ["ls-remote", "--exit-code", "--heads", "origin", branchName], {
-		ignoreReturnCode: true
-	}));
+	const branchExists = !(await exec.exec(
+		"git",
+		["ls-remote", "--exit-code", "--heads", "origin", branchName],
+		{
+			ignoreReturnCode: true,
+		},
+	));
 
 	// create new branch for PR
 	await exec.exec("git", ["checkout", "-b", `${branchName}`]);
 
 	if (branchExists) {
 		// check if our local working copy is different from the remote branch
-		const isChanged = !(await exec.exec("git", ["diff", "--exit-code", `origin/${branchName}`, "--", "docs/"], {
-			ignoreReturnCode: true
-		}));
+		const isChanged = !(await exec.exec(
+			"git",
+			["diff", "--exit-code", `origin/${branchName}`, "--", "docs/"],
+			{
+				ignoreReturnCode: true,
+			},
+		));
 		if (isChanged && !!prNumber) {
-			console.log(`We have no local changes compared to the remote branch and PR exists, exiting...`);
+			console.log(
+				`We have no local changes compared to the remote branch and PR exists, exiting...`,
+			);
 			return;
 		}
 
@@ -60,18 +72,32 @@ const assignees = [
 	}
 
 	// Would the real Al Calzone please stand up?
-	await exec.exec("git", ["config", "--global", "user.email", "d.griesel@gmx.net"]);
+	await exec.exec("git", [
+		"config",
+		"--global",
+		"user.email",
+		"d.griesel@gmx.net",
+	]);
 	await exec.exec("git", ["config", "--global", "user.name", "Al Calzone"]);
-	
+
 	// Create a commit
 	await exec.exec("git", ["add", "."]);
-	await exec.exec("git", ["commit", "-m", "docs: update typed documentation"]);
+	await exec.exec("git", [
+		"commit",
+		"-m",
+		"docs: update typed documentation",
+	]);
 
 	// And push it (real good)
 	if (branchExists) {
 		await exec.exec("git", ["push", "-f"]);
 	} else {
-		await exec.exec("git", ["push", "--set-upstream", "origin", branchName]);
+		await exec.exec("git", [
+			"push",
+			"--set-upstream",
+			"origin",
+			branchName,
+		]);
 	}
 
 	if (!prNumber) {
@@ -87,17 +113,21 @@ const assignees = [
 		prNumber = pr.data.number;
 	}
 	// Request review and add assignee
-	await octokit.pulls.requestReviewers({
-		...options,
-		pull_number: prNumber,
-		reviewers,
-	});
-	await octokit.issues.addAssignees({
-		...options,
-		issue_number: prNumber,
-		assignees,
-	})
-})().catch(e => {
+	if (reviewers.length) {
+		await octokit.pulls.requestReviewers({
+			...options,
+			pull_number: prNumber,
+			reviewers,
+		});
+	}
+	if (assignees.length) {
+		await octokit.issues.addAssignees({
+			...options,
+			issue_number: prNumber,
+			assignees,
+		});
+	}
+})().catch((e) => {
 	console.error(e);
 	process.exit(1);
 });
