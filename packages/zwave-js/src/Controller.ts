81b785dea1d95da84b0e6e1c820da8554205b15b
Paulus Schoutsen <balloob@gmail.com>
2021-01-17 21:24:09 +0100

    Doing the bots work

--- packages/zwave-js/src/Controller.ts
+++ packages/zwave-js/src/Controller.ts
@@ -1,6 +1,6 @@
 export type {
+	ControllerEvents,
 	HealNodeStatus,
 	ZWaveController,
-	ControllerEvents,
 } from "./lib/controller/Controller";
 export type { ZWaveLibraryTypes } from "./lib/controller/ZWaveLibraryTypes";
