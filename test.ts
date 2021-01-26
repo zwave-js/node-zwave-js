13f4ace92daccf3c2ca653da39ea7b49fb9c2ce3
Dominic Griesel <d.griesel@gmx.net>
2021-01-26 18:09:14 +0100

    chore: remove test script

--- test.ts
+++
@@ -1,6 +0,0 @@
-import { loadDeviceIndexInternal } from "./packages/config/src/Devices";
-void (async () => {
-	console.time("load");
-	await loadDeviceIndexInternal();
-	console.timeEnd("load");
-})();
