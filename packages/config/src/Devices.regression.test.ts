468cdaeff1769ab774cefdb020510a0a23d04842
AlCalzone <d.griesel@gmx.net>
2021-01-29 14:53:09 +0100

    fix(test): regenerate device index once before all tests (#1528)

--- packages/config/src/Devices.regression.test.ts
+++ packages/config/src/Devices.regression.test.ts
@@ -2,14 +2,8 @@ import { ConfigManager } from "./ConfigManager";
 
 describe("lib/config/Devices", () => {
 	describe("lookupDevice (regression tests)", () => {
-		let configManager: ConfigManager;
-
-		beforeAll(async () => {
-			configManager = new ConfigManager();
-			await configManager.loadDeviceIndex();
-		}, 60000);
-
 		it("Z-TRM3 with commandClasses.add compat should work", async () => {
+			const configManager = new ConfigManager();
 			const config = await configManager.lookupDevice(
 				0x019b,
 				0x0003,
