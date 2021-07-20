ec6aa4232148258279842bb263937362f3a02714
Dominic Griesel <d.griesel@gmx.net>
2021-07-20 13:59:05 +0200

    fix: mark endpoint indizes value as internal

--- packages/zwave-js/src/lib/commandclass/MultiChannelCC.ts
+++ packages/zwave-js/src/lib/commandclass/MultiChannelCC.ts
@@ -306,6 +306,7 @@ export class MultiChannelCC extends CommandClass {
 
 	public constructor(driver: Driver, options: CommandClassOptions) {
 		super(driver, options);
+		this.registerValue(getEndpointIndizesValueId().property, true);
 		this.registerValue(getEndpointCCsValueId(0).property, true);
 		this.registerValue(getEndpointDeviceClassValueId(0).property, true);
 	}
