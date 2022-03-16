1397c21c6f9235cebfb66812cc0141888f7a1e72
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-03-16 10:28:47 -0400

    feat: export LogContext types (#4378)
    Co-authored-by: Dominic Griesel <d.griesel@gmx.net>

--- packages/zwave-js/src/Controller.ts
+++ packages/zwave-js/src/Controller.ts
@@ -14,4 +14,10 @@ export {
 	TXReport,
 } from "./lib/controller/SendDataShared";
 export type { ZWaveLibraryTypes } from "./lib/controller/ZWaveLibraryTypes";
+export {
+	ControllerLogContext,
+	ControllerNodeLogContext,
+	ControllerSelfLogContext,
+	ControllerValueLogContext,
+} from "./lib/log/Controller";
 export { RFRegion } from "./lib/serialapi/misc/SerialAPISetupMessages";
--- packages/zwave-js/src/Driver.ts
+++ packages/zwave-js/src/Driver.ts
@@ -2,6 +2,7 @@ export { Driver, libName, libVersion } from "./lib/driver/Driver";
 export type { SendMessageOptions } from "./lib/driver/Driver";
 export type { FileSystem } from "./lib/driver/FileSystem";
 export type { ZWaveOptions } from "./lib/driver/ZWaveOptions";
+export { DriverLogContext } from "./lib/log/Driver";
 export {
 	FunctionType,
 	MessagePriority,
