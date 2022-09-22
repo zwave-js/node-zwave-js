b8dd7d15a5f028af006a7ed45e8f436c6e46bda0
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-09-22 13:41:26 +0200

    fix: remove now unnecessary snapshot resolver

--- jest.config.js
+++ jest.config.js
@@ -27,6 +27,4 @@ module.exports = {
 	},
 	// Help jest resolve the compiled files which are referenced inside publishConfig
 	resolver: "./test/jest.moduleResolver.js",
-	// Place snapshots next to test files
-	snapshotResolver: "<rootDir>/test/jest.snapshotResolver.js",
 };
