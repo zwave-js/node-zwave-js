d59e597bd989268c15c3371dc894c815d14fc5e5
Ikcelaks <ikcelaks@gmail.com>
2023-01-02 06:28:59 -0500

    fix: don't overwrite known Wake Up CC version (#5261)
    Co-authored-by: Dominic Griesel <dominic.griesel@nabucasa.com>
    fixes https://github.com/zwave-js/node-zwave-js/issues/5262

--- packages/zwave-js/src/lib/node/Node.ts
+++ packages/zwave-js/src/lib/node/Node.ts
@@ -2913,10 +2913,12 @@ protocol version:      ${this.protocolVersion}`;
 		// It can happen that the node has not told us that it supports the Wake Up CC
 		// https://sentry.io/share/issue/6a681729d7db46d591f1dcadabe8d02e/
 		// To avoid a crash, mark it as supported
-		this.addCC(CommandClasses["Wake Up"], {
-			isSupported: true,
-			version: 1,
-		});
+		if (this.getCCVersion(CommandClasses["Wake Up"]) === 0) {
+			this.addCC(CommandClasses["Wake Up"], {
+				isSupported: true,
+				version: 1,
+			});
+		}
 
 		this.markAsAwake();
 
