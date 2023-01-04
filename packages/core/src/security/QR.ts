7875f7bb87e869bb943f01726059ca8db90fb72b
AlCalzone <dominic.griesel@nabucasa.com>
2023-01-04 12:38:41 +0100

    fix: trim surrounding whitespace from QR codes (#5295)

--- packages/core/src/security/QR.ts
+++ packages/core/src/security/QR.ts
@@ -219,6 +219,9 @@ function parseTLV(qr: string): {
 
 /** Parses a string that has been decoded from a Z-Wave (S2 or SmartStart) QR code */
 export function parseQRCodeString(qr: string): QRProvisioningInformation {
+	// Trim off whitespace that might have been copied by accident
+	qr = qr.trim();
+	// Validate the QR code
 	if (!qr.startsWith("90")) fail("must start with 90");
 	if (qr.length < minQRCodeLength) fail("too short");
 	if (!onlyDigitsRegex.test(qr)) fail("contains invalid characters");
@@ -229,7 +232,7 @@ export function parseQRCodeString(qr: string): QRProvisioningInformation {
 	const checksum = readUInt16(qr, 4);
 	// The checksum covers the remaining data
 	const hash = createHash("sha1");
-	hash.update(Buffer.from(qr.substr(9), "ascii"));
+	hash.update(Buffer.from(qr.slice(9), "ascii"));
 	const expectedChecksum = hash.digest().readUInt16BE(0);
 	if (checksum !== expectedChecksum) fail("invalid checksum");
 
@@ -265,7 +268,7 @@ export function parseQRCodeString(qr: string): QRProvisioningInformation {
 		const {
 			entry: { type, ...data },
 			charsRead,
-		} = parseTLV(qr.substr(offset));
+		} = parseTLV(qr.slice(offset));
 		offset += charsRead;
 
 		if (type === ProvisioningInformationType.ProductId) {
