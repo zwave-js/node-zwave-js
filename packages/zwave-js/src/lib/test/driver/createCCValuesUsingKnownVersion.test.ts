7f7ca9dd69025a49dc3b700d837d24a42254abed
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-02-07 13:29:52 +0100

    test: reproduce issue

---
+++ packages/zwave-js/src/lib/test/driver/createCCValuesUsingKnownVersion.test.ts
@@ -0,0 +1,71 @@
+import { BatteryCCReport, BatteryCCValues } from "@zwave-js/cc";
+import { CommandClasses } from "@zwave-js/core";
+import { createMockZWaveRequestFrame } from "@zwave-js/testing";
+import { wait } from "alcalzone-shared/async";
+import path from "path";
+import { integrationTest } from "../integrationTestSuite";
+
+// Repro for https://github.com/zwave-js/node-zwave-js/issues/5252
+// Here, a Battery report is received before the node's CC version is known
+// This caused the version implemented in Z-Wave JS to be used for creating
+// values, which includes some the device does not actually support
+
+integrationTest("CC values are created using the known CC version", {
+	debug: true,
+
+	provisioningDirectory: path.join(
+		__dirname,
+		"fixtures/ccValuesUnknownVersions",
+	),
+
+	nodeCapabilities: {
+		isListening: false,
+		isFrequentListening: false,
+		// 	commandClasses: [
+		// 		{
+		// 			ccId: CommandClasses["Z-Wave Plus Info"],
+		// 			isSupported: true,
+		// 			version: 2,
+		// 		},
+		// 	],
+	},
+
+	testBody: async (driver, node, mockController, mockNode) => {
+		const batteryReport = new BatteryCCReport(mockNode.host, {
+			nodeId: mockController.host.ownNodeId,
+			isLow: true,
+		});
+		await mockNode.sendToController(
+			createMockZWaveRequestFrame(batteryReport),
+		);
+
+		const updatedMetadata: (string | number)[] = [];
+		node.on("metadata updated", (node, args) => {
+			updatedMetadata.push(args.property);
+		});
+
+		await wait(100);
+
+		const levelValue = BatteryCCValues.level;
+		const isLowValue = BatteryCCValues.isLow;
+
+		// The level value should be defined because it is included in the report
+		// The overheating value shouldn't, since the interview is not complete
+		expect(updatedMetadata).toEqual([
+			levelValue.id.property,
+			isLowValue.id.property,
+		]);
+
+		// Also the correct metadata should be returned dynamically
+		const definedProperties = node
+			.getDefinedValueIDs()
+			.filter((id) => id.commandClass === CommandClasses.Battery)
+			.map((id) => id.property);
+		expect(definedProperties).toEqual([
+			levelValue.id.property,
+			isLowValue.id.property,
+		]);
+
+		expect(node.getValue(levelValue.id)).toBe(0);
+	},
+});
