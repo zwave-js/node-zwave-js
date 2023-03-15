3f19b85237e4d23598b8f830245c5b8a436d668b
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-03-15 17:08:16 +0100

    fix: also use S2 for non-secure commands

--- packages/zwave-js/src/lib/driver/Driver.ts
+++ packages/zwave-js/src/lib/driver/Driver.ts
@@ -2262,17 +2262,42 @@ export class Driver
 		endpointIndex: number = 0,
 	): boolean {
 		const node = this.controller.nodes.get(nodeId);
-		const endpoint = node?.getEndpoint(endpointIndex);
-		return (
-			node?.isSecure !== false &&
-			// Special case for Basic CC, which we sometimes hide:
-			// A securely included node MAY support the Basic Command Class at the highest security level but it
-			// MUST NOT support the Basic Command Class at any lower security level or non-securely.
-			(ccId === CommandClasses.Basic ||
-				// For all other CCs check if the CC is marked as secure
-				!!(endpoint ?? node)?.isCCSecure(ccId)) &&
-			!!(this.securityManager || this.securityManager2)
-		);
+		// Node is unknown, don't use secure communication
+		if (!node) return false;
+
+		const endpoint = node.getEndpoint(endpointIndex);
+
+		const securityClass = node.getHighestSecurityClass();
+		// Node is not secure, don't use secure communication
+		if (securityClass === undefined || securityClass === SecurityClass.None)
+			return false;
+
+		// Special case for Basic CC, which we sometimes hide:
+		// A securely included node MAY support the Basic Command Class at the highest security level but it
+		// MUST NOT support the Basic Command Class at any lower security level or non-securely.
+		const isBasicCC = ccId === CommandClasses.Basic;
+
+		// Security S2 specs also mandate that all non-securely supported CCs MUST also be supported securely
+		// so we can just shortcut if the node is using S2
+		if (securityClassIsS2(securityClass)) {
+			// Use secure communication if the CC is supported. This avoids silly things like S2-encapsulated pings
+			return (
+				!!this.securityManager2 &&
+				(isBasicCC || (endpoint ?? node).supportsCC(ccId))
+			);
+		}
+
+		// Security S0 can be a little more complicated, with secure and non-secure endpoints
+		if (securityClass === SecurityClass.S0_Legacy) {
+			// Therefore actually check if the CC is marked as secure
+			return (
+				!!this.securityManager &&
+				(isBasicCC || (endpoint ?? node).isCCSecure(ccId))
+			);
+		}
+
+		// We shouldn't be here
+		return false;
 	}
 
 	/** @internal Required for ZWaveApplicationHost */
