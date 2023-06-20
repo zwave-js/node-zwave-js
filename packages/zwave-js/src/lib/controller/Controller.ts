a81ff98439276d51e7f5249a88ebe689242568b9
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-06-20 16:20:46 +0200

    fix: remove weird pre-interview "lifeline" setup

--- packages/zwave-js/src/lib/controller/Controller.ts
+++ packages/zwave-js/src/lib/controller/Controller.ts
@@ -44,8 +44,6 @@ import {
 	dskToString,
 	encodeX25519KeyDERSPKI,
 	indexDBsByNode,
-	isRecoverableZWaveError,
-	isTransmissionError,
 	isValidDSK,
 	isZWaveError,
 	nwiHomeIdFromDSK,
@@ -2107,9 +2105,6 @@ supported CCs: ${nodeInfo.supportedCCs
 					}
 				}
 
-				// Bootstrap the node's lifelines, so it knows where the controller is
-				await this.bootstrapLifeline(newNode);
-
 				// We're done adding this node, notify listeners
 				const result: InclusionResult =
 					bootstrapFailure != undefined
@@ -2212,8 +2207,6 @@ supported CCs: ${nodeInfo.supportedCCs
 				newNode,
 				inclCtrlr,
 			);
-			// Bootstrap the node's lifelines, so it knows where the controller is
-			await this.bootstrapLifeline(newNode);
 
 			// We're done adding this node, notify listeners
 			const result: InclusionResult =
@@ -3003,72 +2996,6 @@ supported CCs: ${nodeInfo.supportedCCs
 		}
 	}
 
-	/** Ensures that the node knows where to reach the controller */
-	private async bootstrapLifeline(node: ZWaveNode): Promise<void> {
-		// If the node was bootstrapped with S2, all these requests must happen securely
-		if (securityClassIsS2(node.getHighestSecurityClass())) {
-			for (const cc of [
-				CommandClasses["Wake Up"],
-				CommandClasses.Association,
-				CommandClasses["Multi Channel Association"],
-				CommandClasses.Version,
-			]) {
-				if (node.supportsCC(cc)) {
-					node.addCC(cc, { secure: true });
-				}
-			}
-		}
-
-		if (node.supportsCC(CommandClasses["Z-Wave Plus Info"])) {
-			// SDS11846: The Z-Wave+ lifeline must be assigned to a node as the very first thing
-			if (
-				node.supportsCC(CommandClasses.Association) ||
-				node.supportsCC(CommandClasses["Multi Channel Association"])
-			) {
-				this.driver.controllerLog.logNode(node.id, {
-					message: `Configuring Z-Wave+ Lifeline association...`,
-					direction: "none",
-				});
-				const ownNodeId = this.driver.controller.ownNodeId!;
-
-				try {
-					if (node.supportsCC(CommandClasses.Association)) {
-						await node.commandClasses.Association.addNodeIds(
-							1,
-							ownNodeId,
-						);
-					} else {
-						await node.commandClasses[
-							"Multi Channel Association"
-						].addDestinations({
-							groupId: 1,
-							endpoints: [{ nodeId: ownNodeId, endpoint: 0 }],
-						});
-					}
-
-					// After setting the association, make sure the node knows how to reach us
-					await this.assignReturnRoute(node.id, ownNodeId);
-				} catch (e) {
-					if (isTransmissionError(e) || isRecoverableZWaveError(e)) {
-						this.driver.controllerLog.logNode(node.id, {
-							message: `Failed to configure Z-Wave+ Lifeline association: ${e.message}`,
-							direction: "none",
-							level: "warn",
-						});
-					} else {
-						throw e;
-					}
-				}
-			} else {
-				this.driver.controllerLog.logNode(node.id, {
-					message: `Cannot configure Z-Wave+ Lifeline association: Node does not support associations...`,
-					direction: "none",
-					level: "warn",
-				});
-			}
-		}
-	}
-
 	/**
 	 * Is called when an AddNode request is received from the controller.
 	 * Handles and controls the inclusion process.
@@ -3364,9 +3291,6 @@ supported CCs: ${nodeInfo.supportedCCs
 							level: "warn",
 						});
 					}
-				} else {
-					// Bootstrap the node's lifelines, so it knows where the controller is
-					await this.bootstrapLifeline(newNode);
 				}
 
 				this.setInclusionState(InclusionState.Idle);
@@ -3535,9 +3459,6 @@ supported CCs: ${nodeInfo.supportedCCs
 							newNode.securityClasses.set(secClass, false);
 						}
 					}
-
-					// Bootstrap the node's lifelines, so it knows where the controller is
-					await this.bootstrapLifeline(newNode);
 
 					// We're done adding this node, notify listeners. This also kicks off the node interview
 					const result: InclusionResult =
