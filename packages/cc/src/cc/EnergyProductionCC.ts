176f53c980bd0118a6d07a8becf46a93c1879023
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-06-27 12:00:09 +0200

    chore: add logging for Energy Production CC

--- packages/cc/src/cc/EnergyProductionCC.ts
+++ packages/cc/src/cc/EnergyProductionCC.ts
@@ -1,5 +1,6 @@
 import {
 	CommandClasses,
+	MessageOrCCLogEntry,
 	MessagePriority,
 	ValueMetadata,
 	encodeFloatWithScale,
@@ -230,6 +231,18 @@ export class EnergyProductionCCReport extends EnergyProductionCC {
 		]);
 		return super.serialize();
 	}
+
+	public toLogEntry(applHost: ZWaveApplicationHost): MessageOrCCLogEntry {
+		return {
+			...super.toLogEntry(applHost),
+			message: {
+				[getEnumMemberName(
+					EnergyProductionParameter,
+					this.parameter,
+				).toLowerCase()]: `${this.value} ${this.scale.unit}`,
+			},
+		};
+	}
 }
 
 interface EnergyProductionCCGetOptions extends CCCommandOptions {
@@ -269,5 +282,17 @@ export class EnergyProductionCCGet extends EnergyProductionCC {
 	public serialize(): Buffer {
 		this.payload = Buffer.from([this.parameter]);
 		return super.serialize();
+	}
+
+	public toLogEntry(applHost: ZWaveApplicationHost): MessageOrCCLogEntry {
+		return {
+			...super.toLogEntry(applHost),
+			message: {
+				parameter: getEnumMemberName(
+					EnergyProductionParameter,
+					this.parameter,
+				),
+			},
+		};
 	}
 }
