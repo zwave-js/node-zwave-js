b262a9ac6f203f5ee9f018b3c9ff30359296f5a7
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-07-18 13:39:14 +0200

    feat: implement async queue

---
+++ packages/shared/src/AsyncQueue.test.ts
@@ -0,0 +1,77 @@
+import { wait } from "alcalzone-shared/async";
+import test from "ava";
+import { AsyncQueue } from "./AsyncQueue";
+
+test("can be iterated over after adding items", async (t) => {
+	const queue = new AsyncQueue<number>();
+
+	queue.add(1, 2, 3);
+	queue.end();
+
+	const actual = new Promise<number[]>(async (resolve) => {
+		const seen: number[] = [];
+		for await (const item of queue) {
+			seen.push(item);
+		}
+		resolve(seen);
+	});
+
+	t.deepEqual(await actual, [1, 2, 3]);
+});
+
+test("can be iterated over when adding items later", async (t) => {
+	const queue = new AsyncQueue<number>();
+
+	const actual = new Promise<number[]>(async (resolve) => {
+		const seen: number[] = [];
+		for await (const item of queue) {
+			seen.push(item);
+		}
+		resolve(seen);
+	});
+
+	queue.add(1, 2, 3);
+	queue.end();
+
+	t.deepEqual(await actual, [1, 2, 3]);
+});
+
+test("can be iterated over when adding items asynchronously", async (t) => {
+	const queue = new AsyncQueue<number>();
+
+	const actual = new Promise<number[]>(async (resolve) => {
+		const seen: number[] = [];
+		for await (const item of queue) {
+			seen.push(item);
+		}
+		resolve(seen);
+	});
+
+	queue.add(1, 2, 3);
+	await wait(100);
+	queue.add(4, 5);
+	queue.end();
+
+	t.deepEqual(await actual, [1, 2, 3, 4, 5]);
+});
+
+test("aborting clears all pending items", async (t) => {
+	const queue = new AsyncQueue<number>();
+
+	const actual = new Promise<number[]>(async (resolve) => {
+		const seen: number[] = [];
+		for await (const item of queue) {
+			seen.push(item);
+			if (seen.length === 3) {
+				await wait(100);
+			}
+		}
+		resolve(seen);
+	});
+
+	queue.add(1, 2, 3, 4, 5);
+	await wait(75);
+	queue.abort();
+
+	t.deepEqual(await actual, [1, 2, 3]);
+});
---
+++ packages/shared/src/AsyncQueue.ts
@@ -0,0 +1,68 @@
+import {
+	createDeferredPromise,
+	type DeferredPromise,
+} from "alcalzone-shared/deferred-promise";
+
+export class AsyncQueue<T> {
+	public add(...items: T[]): void {
+		if (items.length === 0 || this.ended) return;
+
+		// Resolve any pending promises first
+		while (items.length > 0 && this.pending.length > 0) {
+			const promise = this.pending.shift()!;
+			const item = items.shift()!;
+			promise.resolve(item);
+		}
+
+		// Add the remaining items to the backlog
+		this.backlog.push(...items);
+	}
+
+	// A list of items that have been pushed but not pulled
+	private backlog: T[] = [];
+	// A list of Promises that are waiting to be resolved
+	private pending: DeferredPromise<T | undefined>[] = [];
+
+	// Whether the queue was ended
+	private ended = false;
+
+	/** Ends the queue after it has been drained */
+	public end(): void {
+		this.ended = true;
+	}
+
+	/** Ends the queue and discards all pending items */
+	public abort(): void {
+		this.ended = true;
+		this.backlog = [];
+		for (const p of this.pending) {
+			p.resolve(undefined);
+		}
+	}
+
+	public [Symbol.asyncIterator](): AsyncIterator<T> {
+		return {
+			next: async (): Promise<IteratorResult<T>> => {
+				let value: T | undefined;
+
+				if (this.backlog.length > 0) {
+					// If there are items in the backlog, return the first one
+					value = this.backlog.shift();
+				} else if (!this.ended) {
+					// Otherwise create a new promise and add it to the pending list
+					const promise = createDeferredPromise<T | undefined>();
+					this.pending.push(promise);
+					value = await promise;
+				}
+
+				if (value) {
+					// We have a value, return it
+					return { value, done: false };
+				} else {
+					// No value means the queue was ended
+					return { value: undefined, done: true };
+				}
+			},
+		};
+	}
+}
