7be110ec38d971c5d691c890d494cac81a283f5e
Dominic Griesel <d.griesel@gmx.net>
2023-08-23 14:06:07 +0200

    fix: do not reformat package.json and yaml files

--- maintenance/fmt-staged.js
+++ maintenance/fmt-staged.js
@@ -6,6 +6,10 @@ const repoRoot = path.join(__dirname, "..");
 const files = process.argv.slice(2);
 
 for (const file of files) {
+	// Skip package.json and similar
+	if (path.basename(file) === "package.json") continue;
+	if (path.basename(file) === "package-lock.json") continue;
+
 	console.log(`Formatting ${file}...`);
 	const content = fs.readFileSync(file, "utf8");
 	const formatted = formatWithDprint(repoRoot, file, content);
