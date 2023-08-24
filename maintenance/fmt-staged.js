b85e82cd74e086e60e359f976acdbe65fcf0ba0e
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-08-24 14:37:56 +0200

    refactor: just run yarn fmt on everything

--- maintenance/fmt-staged.js
+++ maintenance/fmt-staged.js
@@ -1,17 +1,26 @@
-const fs = require("fs");
 const path = require("path");
-const { formatWithDprint } = require("@zwave-js/fmt");
+
+// Dprint parallelizes formatting and is generally fast enough to run on the whole repo
+// despite lint-staged telling us which files have changed.
 const repoRoot = path.join(__dirname, "..");
+require("child_process").spawnSync("yarn", ["fmt"], {
+	cwd: repoRoot,
+});
 
-const files = process.argv.slice(2);
+// If it ever becomes slow, the approach below works:
 
-for (const file of files) {
-	// Skip package.json and similar
-	if (path.basename(file) === "package.json") continue;
-	if (path.basename(file) === "package-lock.json") continue;
+// const fs = require("fs");
+// const { formatWithDprint } = require("@zwave-js/fmt");
 
-	console.log(`Formatting ${file}...`);
-	const content = fs.readFileSync(file, "utf8");
-	const formatted = formatWithDprint(repoRoot, file, content);
-	fs.writeFileSync(file, formatted, "utf8");
-}
+// const files = process.argv.slice(2);
+
+// for (const file of files) {
+// 	// Skip package.json and similar
+// 	if (path.basename(file) === "package.json") continue;
+// 	if (path.basename(file) === "package-lock.json") continue;
+
+// 	console.log(`Formatting ${file}...`);
+// 	const content = fs.readFileSync(file, "utf8");
+// 	const formatted = formatWithDprint(repoRoot, file, content);
+// 	fs.writeFileSync(file, formatted, "utf8");
+// }
