e8ebb4d8095c1556a42aa25f8dcb24fcedcce060
AlCalzone <dominic.griesel@nabucasa.com>
2023-08-24 14:49:40 +0200

    chore: add precommit hook to format files (#6205)

---
+++ maintenance/fmt-staged.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+// Dprint parallelizes formatting and is generally fast enough to run on the whole repo
+// despite lint-staged telling us which files have changed.
+const repoRoot = path.join(__dirname, "..");
+require("child_process").spawnSync("yarn", ["fmt"], {
+	cwd: repoRoot,
+});
+
+// If it ever becomes slow, the approach below works:
+
+// const fs = require("fs");
+// const { formatWithDprint } = require("@zwave-js/fmt");
+
+// const files = process.argv.slice(2);
+
+// for (const file of files) {
+// 	// Skip package.json and similar
+// 	if (path.basename(file) === "package.json") continue;
+// 	if (path.basename(file) === "package-lock.json") continue;
+
+// 	console.log(`Formatting ${file}...`);
+// 	const content = fs.readFileSync(file, "utf8");
+// 	const formatted = formatWithDprint(repoRoot, file, content);
+// 	fs.writeFileSync(file, formatted, "utf8");
+// }
