0bc1ea04b64b441b406192c1867533fcb10e010b
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-08-25 11:41:55 +0200

    test: add regression test for sleeping nodes

--- packages/zwave-js/src/lib/test/driver/sendCommandProgress.test.ts
+++ packages/zwave-js/src/lib/test/driver/sendCommandProgress.test.ts
@@ -3,6 +3,8 @@ import { CommandClasses, TransactionState } from "@zwave-js/core";
 import sinon from "sinon";
 
 import { getEnumMemberName } from "@zwave-js/shared";
+import { wait } from "alcalzone-shared/async";
+import path from "path";
 import { integrationTest } from "../integrationTestSuite";
 
 integrationTest(
@@ -42,6 +44,79 @@ integrationTest(
 			});
 
 			t.pass();
+		},
+	},
+);
+
+integrationTest(
+	"Communication with sleeping nodes is considered correctly",
+	{
+		// debug: true,
+
+		provisioningDirectory: path.join(
+			__dirname,
+			"fixtures/binarySwitchSleeping",
+		),
+
+		// nodeCapabilities: {
+		// 	isListening: false,
+		// 	isFrequentListening: false,
+		// 	commandClasses: [
+		// 		CommandClasses.Version,
+		// 		CommandClasses["Binary Switch"],
+		// 	],
+		// },
+
+		testBody: async (t, driver, node, mockController, mockNode) => {
+			const states: TransactionState[] = [];
+			const onProgress = sinon.stub().callsFake((progress) => {
+				driver.driverLog.print(
+					`onProgress: ${
+						getEnumMemberName(TransactionState, progress.state)
+					}`,
+				);
+				states.push(progress.state);
+			});
+
+			// Node is asleep, but assumed to be awake
+			mockNode.autoAckControllerFrames = false;
+			node.markAsAwake();
+			const promise = node.setValue(
+				BinarySwitchCCValues.targetValue.id,
+				true,
+				{
+					onProgress,
+				},
+			);
+
+			// Wait for the node to get marked asleep again
+			await new Promise((resolve) => {
+				node.once("sleep", resolve);
+			});
+
+			await wait(200);
+
+			t.deepEqual(states, [
+				TransactionState.Queued,
+				TransactionState.Active,
+				TransactionState.Queued,
+			]);
+
+			// Now the node is awake again
+			mockNode.autoAckControllerFrames = true;
+			node.markAsAwake();
+
+			await promise;
+
+			await wait(200);
+		
+			t.deepEqual(states, [
+				TransactionState.Queued,
+				TransactionState.Active,
+				TransactionState.Queued,
+				TransactionState.Active,
+				TransactionState.Completed,
+			]);
 		},
 	},
 );
