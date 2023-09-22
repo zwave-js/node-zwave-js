35aff23329f6894fe1cfcb8fd2c8ffef611da763
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-09-22 12:02:20 +0200

    chore: migrate forbidden imports check to ESLint

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,11 +1,13 @@
 import { ccAPIValidateArgs } from "./rules/ccapi-validate-args.js";
 import { consistentCCClasses } from "./rules/consistent-cc-classes.js";
 import { noDebugInTests } from "./rules/no-debug-in-tests.js";
+import { noForbiddenImports } from "./rules/no-forbidden-imports.js";
 
 module.exports = {
 	rules: {
 		"no-debug-in-tests": noDebugInTests,
 		"ccapi-validate-args": ccAPIValidateArgs,
 		"consistent-cc-classes": consistentCCClasses,
+		"no-forbidden-imports": noForbiddenImports,
 	},
 };
