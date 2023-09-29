172650b978e1838a29b9ec30db8bec203e462c70
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-09-29 21:53:10 +0200

    chore: enforce param descriptions to be in Sentence case

--- packages/eslint-plugin/src/rules/consistent-config-labels.ts
+++ packages/eslint-plugin/src/rules/consistent-config-labels.ts
@@ -2,6 +2,8 @@ import { type AST } from "jsonc-eslint-parser";
 import { type JSONCRule } from "../utils";
 
 const ROOT = "Program > JSONExpressionStatement > JSONObjectExpression";
+const CONFIG_PARAM =
+	"JSONProperty[key.value='paramInformation'] > JSONArrayExpression > JSONObjectExpression";
 
 function isSurroundedByWhitespace(str: string) {
 	return /^\s/.test(str) || /\s$/.test(str);
@@ -20,6 +22,11 @@ const titleCaseExceptions = [
 	"kW",
 	"kWh",
 	"RFID",
+];
+
+const sentenceCaseIgnored: RegExp[] = [
+	// emphasis:
+	/^NOT$/,
 ];
 
 const titleCaseIgnored: RegExp[] = [
@@ -74,8 +81,72 @@ function toTitleCase(str: string) {
 		if (alwaysUppercase.some((re) => re.test(word))) {
 			return word.toUpperCase();
 		}
-		// Title case the rest
+		// Title Case the rest
 		return word[0].toUpperCase() + word.slice(1);
+	}).join(" ");
+}
+
+function isSentenceCase(str: string) {
+	const words = str.split(" ");
+	return words.every((word, i) => {
+		if (word.length === 0) return true;
+		const isFirstWord = i === 0 || words[i - 1].endsWith(".");
+
+		if (!isFirstWord) {
+			// Exceptions don't apply for the first word
+			const exception = titleCaseExceptions.find(
+				(ex) => ex.toLowerCase() === word.toLowerCase(),
+			);
+			if (exception) return word === exception;
+		}
+		if (
+			titleCaseIgnored.some((re) => re.test(word))
+			|| sentenceCaseIgnored.some((re) => re.test(word))
+		) {
+			return true;
+		}
+		if (alwaysUppercase.some((re) => re.test(word))) {
+			return word === word.toUpperCase();
+		}
+
+		return isFirstWord
+			// First word Uppercase
+			? word[0] === word[0].toUpperCase()
+			// Other words lowercase
+			: word[0] === word[0].toLowerCase();
+	});
+}
+
+function toSentenceCase(str: string) {
+	const words = str.split(" ");
+	return words.map((word, i) => {
+		if (word.length === 0) return word;
+
+		const isFirstWord = i === 0 || words[i - 1].endsWith(".");
+
+		// Return some exceptions as they are defined
+		if (!isFirstWord) {
+			// Exceptions don't apply for the first word in each sentence
+			const exception = titleCaseExceptions.find(
+				(ex) => ex.toLowerCase() === word.toLowerCase(),
+			);
+			if (exception) return exception;
+		}
+		if (
+			titleCaseIgnored.some((re) => re.test(word))
+			|| sentenceCaseIgnored.some((re) => re.test(word))
+		) {
+			return word;
+		}
+		if (alwaysUppercase.some((re) => re.test(word))) {
+			return word.toUpperCase();
+		}
+		// Sentence case the rest
+		return (isFirstWord
+			// First word Uppercase
+			? word[0].toUpperCase()
+			// Other words lowercase
+			: word[0].toLowerCase()) + word.slice(1);
 	}).join(" ");
 }
 
@@ -134,10 +205,39 @@ export const consistentConfigLabels: JSONCRule.RuleModule = {
 					],
 				});
 			},
-			// "JSONProperty[key.value='paramInformation'] > JSONArrayExpression > JSONObjectExpression"(
-			// 	node: AST.JSONObjectExpression,
-			// ) {
-			// },
+
+			// Enforce sentence case for param descriptions
+			[`${CONFIG_PARAM} > JSONProperty[key.value='description']`](
+				node: AST.JSONProperty,
+			) {
+				if (
+					node.value.type !== "JSONLiteral"
+					|| typeof node.value.value !== "string"
+				) return;
+				const value = node.value;
+				if (isSentenceCase(value.value)) return;
+
+				const fixed = toSentenceCase(value.raw.slice(1, -1));
+
+				context.report({
+					loc: node.loc,
+					messageId: "must-be-sentence-case",
+					data: {
+						what: "Parameter descriptions",
+					},
+					suggest: [
+						{
+							messageId: "change-to-fixed",
+							data: { fixed },
+							fix: (fixer) =>
+								fixer.replaceTextRange(
+									value.range,
+									`"${fixed}"`,
+								),
+						},
+					],
+				});
+			},
 		};
 	},
 	meta: {
@@ -152,6 +252,7 @@ export const consistentConfigLabels: JSONCRule.RuleModule = {
 			"no-surrounding-whitespace":
 				"Leading and trailing whitespace is not allowed",
 			"must-be-title-case": "{{what}} must be in Title Case",
+			"must-be-sentence-case": "{{what}} must be in Sentence case",
 			"change-to-fixed": `Change to "{{fixed}}"`,
 		},
 		type: "problem",
