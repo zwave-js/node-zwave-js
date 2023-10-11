8fcdc4073d52d6b9aa430bf1f225f479d756f5f3
AlCalzone <dominic.griesel@nabucasa.com>
2023-10-11 10:49:41 +0200

    chore: add ESLint rule to enforce consistency in device config files and prevent some common errors (#6345)

---
+++ packages/eslint-plugin/src/rules/no-surrounding-whitespace.ts
@@ -0,0 +1,45 @@
+import { type AST } from "jsonc-eslint-parser";
+import { type JSONCRule } from "../utils";
+
+function isSurroundedByWhitespace(str: string) {
+	return /^\s/.test(str) || /\s$/.test(str);
+}
+
+export const noSurroundingWhitespace: JSONCRule.RuleModule = {
+	create(context) {
+		if (!context.parserServices.isJSON) {
+			return {};
+		}
+
+		return {
+			// Disallow surrounding whitespace
+			JSONLiteral(node: AST.JSONLiteral) {
+				if (typeof node.value !== "string") return;
+				if (!isSurroundedByWhitespace(node.value)) return;
+
+				context.report({
+					loc: node.loc,
+					messageId: "no-surrounding-whitespace",
+					fix: (fixer) =>
+						fixer.replaceTextRange(
+							node.range,
+							`"${node.raw.slice(1, -1).trim()}"`,
+						),
+				});
+			},
+		};
+	},
+	meta: {
+		docs: {
+			description:
+				`Prevents strings in configuration files to be surrounded by whitespace`,
+		},
+		fixable: "code",
+		schema: [],
+		messages: {
+			"no-surrounding-whitespace":
+				"Leading and trailing whitespace is not allowed",
+		},
+		type: "problem",
+	},
+};
