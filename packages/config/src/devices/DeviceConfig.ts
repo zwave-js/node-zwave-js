f5b6a6437c46ecb7d3ac74a4fd5982afcba5ae76
XLDom <90308906+XLDom@users.noreply.github.com>
2023-10-18 14:00:12 +0200

    Update DeviceConfig.ts

--- packages/config/src/devices/DeviceConfig.ts
+++ packages/config/src/devices/DeviceConfig.ts
@@ -866,6 +866,7 @@ export class DeviceConfig {
 					"removeEndpoints",
 					"treatBasicSetAsEvent",
 					"treatMultilevelSwitchSetAsEvent",
+					"treatSetAsReport",
 				] as const
 			) {
 				if (this.compat[prop] != undefined) {
