3ee65af9cbe08621c733d8a6b5a5e6e097e46939
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-04-10 09:28:01 +0200

    chore: add EU LR region

--- packages/core/src/capabilities/RFRegion.ts
+++ packages/core/src/capabilities/RFRegion.ts
@@ -3,11 +3,14 @@ export enum RFRegion {
 	"USA" = 0x01,
 	"Australia/New Zealand" = 0x02,
 	"Hong Kong" = 0x03,
+	// 0x04 is deprecated
 	"India" = 0x05,
 	"Israel" = 0x06,
 	"Russia" = 0x07,
 	"China" = 0x08,
 	"USA (Long Range)" = 0x09,
+	// 0x0a is deprecated
+	"EU (Long Range)" = 0x0b,
 	"Japan" = 0x20,
 	"Korea" = 0x21,
 	"Unknown" = 0xfe,
