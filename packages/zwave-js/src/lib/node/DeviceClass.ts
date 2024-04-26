76e8b352d15dd80db5d5dfbdbd334903a94643fc
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-04-26 12:44:38 +0200

    fix: re-introduce mandatory CCs to DeviceClass as deprecated

--- packages/zwave-js/src/lib/node/DeviceClass.ts
+++ packages/zwave-js/src/lib/node/DeviceClass.ts
@@ -4,6 +4,7 @@ import type {
 	GenericDeviceClass,
 	SpecificDeviceClass,
 } from "@zwave-js/config";
+import { type CommandClasses } from "@zwave-js/core";
 import type { JSONObject } from "@zwave-js/shared";
 
 export class DeviceClass {
@@ -24,6 +25,16 @@ export class DeviceClass {
 	public readonly basic: BasicDeviceClass;
 	public readonly generic: GenericDeviceClass;
 	public readonly specific: SpecificDeviceClass;
+
+	/** @deprecated This property is no longer in use and contains no information. */
+	public get mandatorySupportedCCs(): readonly CommandClasses[] {
+		return [];
+	}
+
+	/** @deprecated This property is no longer in use and contains no information. */
+	public get mandatoryControlledCCs(): readonly CommandClasses[] {
+		return [];
+	}
 
 	public toJSON(): JSONObject {
 		return {
