a71dbfcb8b231db9ef5d327fd257f82a514dfb39
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-06-07 11:34:22 +0200

    feat: support creating a mock node from a node dump

--- packages/zwave-js/src/mockServer.ts
+++ packages/zwave-js/src/mockServer.ts
@@ -1,3 +1,4 @@
+import { CommandClasses } from "@zwave-js/core";
 import type { ZWaveSerialPort } from "@zwave-js/serial";
 import {
 	type MockPortBinding,
@@ -10,13 +11,18 @@ import {
 	MockNode,
 	type MockNodeBehavior,
 	type MockNodeOptions,
+	type PartialCCCapabilities,
+	getDefaultMockEndpointCapabilities,
+	getDefaultMockNodeCapabilities,
 } from "@zwave-js/testing";
 import { createDeferredPromise } from "alcalzone-shared/deferred-promise";
 import { type AddressInfo, type Server, createServer } from "node:net";
 import {
+	ProtocolVersion,
 	createDefaultMockControllerBehaviors,
 	createDefaultMockNodeBehaviors,
 } from "./Utils";
+import { type CommandClassDump, type NodeDump } from "./lib/node/Dump";
 
 export type MockServerControllerOptions =
 	& Pick<
@@ -183,4 +189,134 @@ function prepareMocks(
 		mockController,
 		mockNodes,
 	};
+}
+
+export function createMockNodeOptionsFromDump(
+	dump: NodeDump,
+): MockServerNodeOptions {
+	const ret: MockServerNodeOptions = {
+		id: dump.id,
+	};
+
+	ret.capabilities = getDefaultMockNodeCapabilities();
+
+	if (typeof dump.isListening === "boolean") {
+		ret.capabilities.isListening = dump.isListening;
+	}
+	if (dump.isFrequentListening !== "unknown") {
+		ret.capabilities.isFrequentListening = dump.isFrequentListening;
+	}
+	if (typeof dump.isRouting === "boolean") {
+		ret.capabilities.isRouting = dump.isRouting;
+	}
+	if (typeof dump.supportsBeaming === "boolean") {
+		ret.capabilities.supportsBeaming = dump.supportsBeaming;
+	}
+	if (typeof dump.supportsSecurity === "boolean") {
+		ret.capabilities.supportsSecurity = dump.supportsSecurity;
+	}
+	if (typeof dump.supportedDataRates === "boolean") {
+		ret.capabilities.supportedDataRates = dump.supportedDataRates;
+	}
+	if ((ProtocolVersion as any)[dump.protocol] !== undefined) {
+		ret.capabilities.protocolVersion =
+			(ProtocolVersion as any)[dump.protocol];
+	}
+
+	if (dump.deviceClass !== "unknown") {
+		ret.capabilities.basicDeviceClass = dump.deviceClass.basic.key;
+		ret.capabilities.genericDeviceClass = dump.deviceClass.generic.key;
+		ret.capabilities.specificDeviceClass = dump.deviceClass.specific.key;
+	}
+
+	ret.capabilities.firmwareVersion = dump.fingerprint.firmwareVersion;
+	ret.capabilities.manufacturerId = parseInt(
+		dump.fingerprint.manufacturerId,
+		16,
+	);
+	ret.capabilities.productType = parseInt(dump.fingerprint.productType, 16);
+	ret.capabilities.productId = parseInt(dump.fingerprint.productId, 16);
+
+	for (const [ccName, ccDump] of Object.entries(dump.commandClasses)) {
+		const ccId = (CommandClasses as any)[ccName];
+		if (ccId == undefined) continue;
+		// FIXME: Security encapsulation is not supported yet in mocks
+		if (
+			ccId === CommandClasses.Security
+			|| ccId === CommandClasses["Security 2"]
+		) {
+			continue;
+		}
+
+		ret.capabilities.commandClasses ??= [];
+		ret.capabilities.commandClasses.push(
+			createCCCapabilitiesFromDump(ccId, ccDump),
+		);
+	}
+
+	if (dump.endpoints) {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		for (const [indexStr, endpointDump] of Object.entries(dump.endpoints)) {
+			// FIXME: The mocks expect endpoints to be consecutive
+			// const index = parseInt(indexStr);
+
+			const epCaps = getDefaultMockEndpointCapabilities(
+				// @ts-expect-error We are initializing the device classes above
+				ret.capabilities,
+			);
+			let epCCs: PartialCCCapabilities[] | undefined;
+			if (endpointDump.deviceClass !== "unknown") {
+				epCaps.genericDeviceClass =
+					endpointDump.deviceClass.generic.key;
+				epCaps.specificDeviceClass =
+					endpointDump.deviceClass.specific.key;
+			}
+
+			for (
+				const [ccName, ccDump] of Object.entries(
+					endpointDump.commandClasses,
+				)
+			) {
+				const ccId = (CommandClasses as any)[ccName];
+				if (ccId == undefined) continue;
+				// FIXME: Security encapsulation is not supported yet in mocks
+				if (
+					ccId === CommandClasses.Security
+					|| ccId === CommandClasses["Security 2"]
+				) {
+					continue;
+				}
+
+				epCCs ??= [];
+				epCCs.push(
+					createCCCapabilitiesFromDump(ccId, ccDump),
+				);
+			}
+
+			ret.capabilities.endpoints ??= [];
+			ret.capabilities.endpoints.push({
+				...epCaps,
+				commandClasses: epCCs,
+			});
+		}
+	}
+
+	return ret;
+}
+
+function createCCCapabilitiesFromDump(
+	ccId: CommandClasses,
+	dump: CommandClassDump,
+): PartialCCCapabilities {
+	const ret: PartialCCCapabilities = {
+		ccId,
+		isSupported: dump.isSupported,
+		isControlled: dump.isControlled,
+		secure: dump.secure,
+		version: dump.version,
+	};
+
+	// TODO: Parse CC specific info from values
+
+	return ret;
 }
