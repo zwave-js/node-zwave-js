c4df30664a4a278f97e35476c707f9fd67b7b22e
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-07-11 12:17:02 +0200

    fix: only mark Basic CC as controlled if compat flags are set

--- packages/cc/src/cc/VersionCC.ts
+++ packages/cc/src/cc/VersionCC.ts
@@ -480,7 +480,6 @@ export class VersionCC extends CommandClass {
 					// for which support is determined later.
 					if (cc === CommandClasses.Basic) {
 						endpoint.addCC(cc, {
-							isControlled: true,
 							version: supportedVersion,
 						});
 					} else {
