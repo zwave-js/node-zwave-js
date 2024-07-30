bd3e9040e2b5e1243052986a699d4129ebf5f90d
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-07-30 15:01:04 +0200

    chore: update list of existing CCs

---
+++ maintenance/parse-zwxml.ts
@@ -0,0 +1,63 @@
+import { getCCName } from "@zwave-js/core";
+import fs from "node:fs/promises";
+import xml2js from "xml2js";
+
+const xmlParserOptions_default: xml2js.ParserOptions = {
+	normalize: true,
+	// Don't separate xml attributes from children
+	mergeAttrs: true,
+	// We normalize to arrays where necessary, no need to do it globally
+	explicitArray: false,
+	explicitRoot: false,
+	attrValueProcessors: [
+		function parseVersion(value, name) {
+			if (name === "version") return parseInt(value, 10);
+			return value;
+		},
+	],
+};
+
+async function main() {
+	const { cmd_class } = await xml2js
+		.parseStringPromise(
+			await fs.readFile("zwave.xml", "utf8"),
+			xmlParserOptions_default,
+		);
+
+	cmd_class.sort((a, b) => a.name.localeCompare(b.name));
+
+	const ccs = new Map<string, any>();
+	for (const cc of cmd_class) {
+		if (ccs.has(cc.key) && ccs.get(cc.key).version > cc.version) {
+			continue;
+		}
+		ccs.set(cc.key, cc);
+		const ccId = parseInt(cc.key, 16);
+		let ccName = getCCName(ccId);
+		if (ccName.startsWith("unknown")) {
+			ccName = cc.help.replace(/Command Class /i, "");
+		}
+		cc.name = ccName;
+	}
+
+	const allCCs = [...ccs.values()].sort((a, b) =>
+		a.name.localeCompare(b.name)
+	);
+
+	const implemented = `{
+	${
+		allCCs.map((cc) => {
+			let info = "";
+			if (cc.comment?.toLowerCase().includes("deprecated")) {
+				info = ", deprecated: true";
+			} else if (cc.comment?.toLowerCase().includes("obsoleted")) {
+				info = ", obsolete: true";
+			}
+			return `	"${cc.key}": { version: ${cc.version}${info} }, // ${cc.name}`;
+		}).join("\n")
+	}
+}`;
+
+	await fs.writeFile("implemented.txt", implemented);
+}
+void main();
--- packages/maintenance/src/implementedCCs.ts
+++ packages/maintenance/src/implementedCCs.ts
@@ -204,115 +204,116 @@ const ccVersions: Record<
 	| { version: string | number; deprecated?: boolean; obsolete?: boolean }
 	| undefined
 > = {
-	"0x9C": { version: 1, deprecated: true },
-	"0x9D": { version: 1 },
-	"0x5D": { version: 3 },
-	"0x7e": { version: 1 },
-	"0x22": { version: 1 },
-	"0x85": { version: 3 },
-	"0x9B": { version: 1 },
-	"0x59": { version: 3 },
-	"0xA1": { version: 1 },
-	"0xA2": { version: 1 },
-	"0x66": { version: 1 },
-	"0x20": { version: 2 },
-	"0x36": { version: 1 },
-	"0x80": { version: 3 },
-	"0x30": { version: 2, deprecated: true },
-	"0x25": { version: 2 },
-	"0x46": { version: 1, deprecated: true },
-	"0x5B": { version: 3 },
-	"0x81": { version: 1 },
-	"0x33": { version: 3 },
-	"0x70": { version: 4 },
-	"0x21": { version: 1 },
-	"0x56": { version: 1, deprecated: true },
-	"0x3A": { version: 1 },
-	"0x3B": { version: 1 },
-	"0x5A": { version: 1 },
-	"0x62": { version: 4 },
-	"0x4C": { version: 1 },
-	"0x90": { version: 1 },
-	"0x6F": { version: 1 },
-	"0x7A": { version: 7 },
-	"0xA3": { version: 1 },
-	"0x8C": { version: 1 },
-	"0x7B": { version: 1, deprecated: true },
-	"0x37": { version: 1 },
-	"0x39": { version: 1 },
-	"0x6D": { version: 2 },
-	"0x6E": { version: 1 },
-	"0x64": { version: 2 },
-	"0x74": { version: 1 },
-	"0x87": { version: 4 },
-	"0x5C": { version: 1 },
-	"0xA0": { version: 1 },
-	"0x6B": { version: 1 },
-	"0x89": { version: 1 },
-	"0x76": { version: 1, deprecated: true },
-	"0x69": { version: 2 },
-	"0x91": { version: 1 },
-	"0x72": { version: 2 },
-	"0x32": { version: 6 },
-	"0x3C": { version: 1 },
-	"0x3D": { version: 3 },
-	"0x3E": { version: 1 },
-	"0x60": { version: 4 },
-	"0x8E": { version: 4 },
-	"0x8F": { version: 1 },
-	"0x31": { version: 11 },
-	"0x26": { version: 4 },
-	"0x29": { version: 1, deprecated: true },
-	"0x4D": { version: 2 },
-	"0x34": { version: 3 },
-	"0x67": { version: 2 },
-	"0x52": { version: 3 },
-	"0x00": { version: 1 },
-	"0x77": { version: 1 },
-	"0x78": { version: 1 },
-	"0x71": { version: 8 },
-	"0x73": { version: 1 },
-	"0x3F": { version: 1 },
-	"0x41": { version: 1 },
-	"0x88": { version: 1, deprecated: true },
-	"0x75": { version: 2 },
-	"0x35": { version: 1, deprecated: true },
-	"0x48": { version: 1 },
-	"0x49": { version: 1 },
-	"0x2B": { version: 1 },
-	"0x2C": { version: 1 },
-	"0x2D": { version: 1 },
-	"0x53": { version: 4 },
-	"0x4E": { version: 3, deprecated: true },
-	"0x93": { version: 2 },
-	"0x92": { version: 2 },
-	"0x98": { version: 1 },
-	"0x9F": { version: 1 },
-	"0x94": { version: 4 },
-	"0x79": { version: 2 },
-	"0x6C": { version: 2 },
-	"0x4A": { version: 1 },
-	"0x4B": { version: 1 },
-	"0x44": { version: 5 },
-	"0x45": { version: 2 },
-	"0x40": { version: 3 },
-	"0x42": { version: 2 },
-	"0x47": { version: 1 },
-	"0x43": { version: 3 },
-	"0x8A": { version: 2 },
-	"0x8B": { version: 1 },
-	"0x55": { version: 2 },
-	"0x63": { version: 2 },
-	"0x86": { version: 3 },
-	"0x84": { version: 3 },
-	"0x6A": { version: 1 },
-	"0x23": { version: 4 },
-	"0x4F": { version: 1 },
-	"0x5F": { version: 1 },
-	"0x68": { version: 1 },
-	"0x58": { version: 1 },
-	"0x61": { version: 1 },
-	"0x5E": { version: 2 },
+	"0x9C": { version: 1, deprecated: true }, // Alarm Sensor
+	"0x9D": { version: 1 }, // Alarm Silence
+	"0x5D": { version: 3, obsolete: true }, // Anti-Theft
+	"0x7E": { version: 1, deprecated: true }, // Anti-Theft Unlock
+	"0x22": { version: 1 }, // Application Status
+	"0x85": { version: 4 }, // Association
+	"0x9B": { version: 1 }, // Association Command Configuration
+	"0x59": { version: 3 }, // Association Group Information
+	"0xA1": { version: 1 }, // Authentication
+	"0xA2": { version: 1 }, // Authentication Media Write
+	"0x66": { version: 1 }, // Barrier Operator
+	"0x20": { version: 2 }, // Basic
+	"0x36": { version: 1 }, // Basic Tariff Information
+	"0x80": { version: 3 }, // Battery
+	"0x30": { version: 2, deprecated: true }, // Binary Sensor
+	"0x25": { version: 2 }, // Binary Switch
+	"0x5B": { version: 3 }, // Central Scene
+	"0x46": { version: 1, deprecated: true }, // Climate Control Schedule
+	"0x81": { version: 1 }, // Clock
+	"0x33": { version: 3 }, // Color Switch
+	"0x70": { version: 4 }, // Configuration
+	"0x21": { version: 1 }, // Controller Replication
+	"0x56": { version: 1, deprecated: true }, // CRC-16 Encapsulation
+	"0x3A": { version: 1 }, // Demand Control Plan Configuration
+	"0x3B": { version: 1 }, // Demand Control Plan Monitor
+	"0x5A": { version: 1 }, // Device Reset Locally
+	"0x62": { version: 4 }, // Door Lock
+	"0x4C": { version: 1 }, // Door Lock Logging
+	"0x90": { version: 1 }, // Energy Production
+	"0x6F": { version: 1 }, // Entry Control
+	"0x7A": { version: 8 }, // Firmware Update Meta Data
+	"0xA3": { version: 1 }, // Generic Schedule
+	"0x8C": { version: 1 }, // Geographic Location
+	"0x7B": { version: 1, deprecated: true }, // Grouping Name
+	"0x39": { version: 1 }, // HRV Control
+	"0x37": { version: 1 }, // HRV Status
+	"0x6D": { version: 2 }, // Humidity Control Mode
+	"0x6E": { version: 1 }, // Humidity Control Operating State
+	"0x64": { version: 2 }, // Humidity Control Setpoint
+	"0x74": { version: 1 }, // Inclusion Controller
+	"0x87": { version: 4 }, // Indicator
+	"0x5C": { version: 1 }, // IP Association
+	"0xA0": { version: 1 }, // IR Repeater
+	"0x6B": { version: 1 }, // Irrigation
+	"0x89": { version: 1 }, // Language
+	"0x76": { version: 1, deprecated: true }, // Lock
+	"0x69": { version: 2 }, // Mailbox
+	"0x91": { version: 1, obsolete: true }, // Manufacturer Proprietary
+	"0x72": { version: 2 }, // Manufacturer Specific
+	"0x32": { version: 6 }, // Meter
+	"0x3C": { version: 1 }, // Meter Table Configuration
+	"0x3D": { version: 3 }, // Meter Table Monitor
+	"0x60": { version: 4 }, // Multi Channel
+	"0x8E": { version: 5 }, // Multi Channel Association
+	"0x8F": { version: 1 }, // Multi Command
+	"0x31": { version: 11 }, // Multilevel Sensor
+	"0x26": { version: 4 }, // Multilevel Switch
+	"0x29": { version: 1, deprecated: true }, // Multilevel Toggle Switch
+	"0x4D": { version: 2 }, // Network Management Basic Node
+	"0x34": { version: 4 }, // Network Management Inclusion
+	"0x67": { version: 4 }, // Network Management Installation and Maintenance
+	"0x52": { version: 4 }, // Network Management Proxy
+	"0x00": { version: 1 }, // No Operation
+	"0x77": { version: 1 }, // Node Naming and Location
+	"0x78": { version: 1 }, // Node Provisioning
+	"0x71": { version: 9 }, // Notification
+	"0x73": { version: 1 }, // Powerlevel
+	"0x3F": { version: 1 }, // Prepayment
+	"0x41": { version: 1 }, // Prepayment Encapsulation
+	"0x75": { version: 2 }, // Protection
+	"0x35": { version: 1, deprecated: true }, // Pulse Meter
+	"0x48": { version: 1 }, // Rate Table Configuration
+	"0x49": { version: 1 }, // Rate Table Monitor
+	"0x2B": { version: 1 }, // Scene Activation
+	"0x2C": { version: 1 }, // Scene Actuator Configuration
+	"0x2D": { version: 1 }, // Scene Controller Configuration
+	"0x53": { version: 4 }, // Schedule
+	"0x4E": { version: 4 }, // Schedule Entry Lock
+	"0x93": { version: 2 }, // Screen Attributes
+	"0x92": { version: 2 }, // Screen Meta Data
+	"0x98": { version: 1 }, // Security
+	"0x9F": { version: 2 }, // Security 2
+	"0x94": { version: 4 }, // Simple AV Control
+	"0x79": { version: 2 }, // Sound Switch
+	"0x6C": { version: 2 }, // Supervision
+	"0x4A": { version: 1 }, // Tariff Table Configuration
+	"0x4B": { version: 1 }, // Tariff Table Monitor
+	"0x44": { version: 5 }, // Thermostat Fan Mode
+	"0x45": { version: 2 }, // Thermostat Fan State
+	"0x40": { version: 3 }, // Thermostat Mode
+	"0x42": { version: 2 }, // Thermostat Operating State
+	"0x47": { version: 1 }, // Thermostat Setback
+	"0x43": { version: 3 }, // Thermostat Setpoint
+	"0x8A": { version: 2 }, // Time
+	"0x8B": { version: 1 }, // Time Parameters
+	"0x55": { version: 2 }, // Transport Service
+	"0x63": { version: 2 }, // User Code
+	"0x83": { version: 1 }, // User Credential
+	"0x86": { version: 3 }, // Version
+	"0x84": { version: 3 }, // Wake Up
+	"0x6A": { version: 1 }, // Window Covering
+	"0x04": { version: 1 }, // Z-Wave Long Range
+	"0x5E": { version: 2 }, // Z-Wave Plus Info
+	"0x01": { version: 1 }, // Z-Wave Protocol
+	"0x23": { version: 4 }, // Z/IP
+	"0x4F": { version: 1 }, // Z/IP 6LoWPAN
+	"0x5F": { version: 1 }, // Z/IP Gateway
+	"0x68": { version: 1 }, // Z/IP Naming and Location
+	"0x58": { version: 2 }, // Z/IP ND
+	"0x61": { version: 1 }, // Z/IP Portal
 };
 
 function getLatestVersion(ccName: string) {
