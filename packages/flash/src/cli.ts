e8bc672545be67d962a8899f24b533bbf9c84af6
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-10-31 10:57:35 +0100

    fix: flash build

--- packages/flash/src/cli.ts
+++ packages/flash/src/cli.ts
@@ -3,6 +3,7 @@ import { wait } from "alcalzone-shared/async";
 import fs from "node:fs/promises";
 import path from "node:path";
 import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import {
 	ControllerFirmwareUpdateStatus,
 	Driver,
@@ -11,7 +12,7 @@ import {
 	guessFirmwareFileFormat,
 } from "zwave-js";
 
-const argv = yargs.parseSync();
+const argv = yargs(hideBin(process.argv)).parseSync();
 const [port, filename] = argv._.map(String);
 
 if (!port || !filename) {
