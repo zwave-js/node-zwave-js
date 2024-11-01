0e627323c14fa1de408d9b30827271b38b99e2a2
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-11-01 22:55:23 +0100

    fix: include pattern once more

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,10 +2,6 @@ import { defaultExclude, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		include: [
-			"packages/**/test/**/*.ts",
-			"**/*.test.ts",
-		],
 		exclude: [
 			...defaultExclude,
 			"build/**",
