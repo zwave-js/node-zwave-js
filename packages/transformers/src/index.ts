eca9d4633515a9412a4100e34f766e5d449205e9
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-11-18 13:05:38 +0100

    refactor: use new transformers

--- packages/transformers/src/index.ts
+++ packages/transformers/src/index.ts
@@ -1,54 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-// function checkGetErrorObject(
-// 	getErrorObject: unknown,
-// ): asserts getErrorObject is (obj: any) => any {
-// 	if (typeof getErrorObject !== "function") {
-// 		throw new Error(
-// 			"This module should not be used in runtime. Instead, use a transformer during compilation.",
-// 		);
-// 	}
-// }
-
-// /**
-//  * Checks if the given argument is assignable to the given type-argument.
-//  *
-//  * @param object object whose type needs to be checked.
-//  * @returns `true` if `object` is assignable to `T`, false otherwise.
-//  * @example
-//    ```
-//    is<number>(42); // -> true
-//    is<number>('foo'); // -> false
-//    ```
-//  */
-// export function is<T>(object: any): object is T;
-// export function is<T>(obj: any, getErrorObject?: (obj: any) => any): obj is T {
-// 	checkGetErrorObject(getErrorObject);
-// 	const errorObject = getErrorObject(obj);
-// 	return errorObject === null;
-// }
-
-// /**
-//  * Creates a function similar to `is<T>` that can be invoked at a later point.
-//  *
-//  * This is useful, for example, if you want to re-use the function multiple times.
-//  *
-//  * @example
-//    ```
-//    const checkNumber = createIs<number>();
-//    checkNumber(42); // -> true
-//    checkNumber('foo'); // -> false
-//    ```
-//  */
-// export function createIs<T>(): (object: any) => object is T;
-// export function createIs<T>(
-// 	getErrorObject = undefined,
-// ): (object: any) => object is T {
-// 	checkGetErrorObject(getErrorObject);
-// 	// @ts-expect-error We're using an internal signature
-// 	return (obj) => is(obj, getErrorObject);
-// }
-
 export interface ValidateArgsOptions {
 	/**
 	 * Enable strict value checks for numeric enums. By default, a numeric enum will accept any number.
@@ -74,6 +25,3 @@ export function validateArgs<T extends (...args: any[]) => any>(
 		);
 	};
 }
-
-import transformer from "./validateArgs/transformer";
-export default transformer;
