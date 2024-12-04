2596135595541433d16db69ab3cb18b718a4adde
Dominic Griesel <dominic.griesel@nabucasa.com>
2024-12-04 12:19:54 +0100

    feat: implement custom state machine

---
+++ packages/core/src/fsm/FSM.ts
@@ -0,0 +1,78 @@
+export interface StateMachineTransition<
+	State extends StateMachineState,
+	Effect = undefined,
+> {
+	effect?: Effect;
+	newState: State;
+	delayMs?: number;
+}
+
+export interface StateMachineState {
+	value: number | string;
+	done?: boolean;
+}
+
+export interface StateMachineInput {
+	input: number | string;
+}
+
+export type StateMachineTransitionMap<
+	State extends StateMachineState,
+	Input extends StateMachineInput,
+	Effect = undefined,
+> = (
+	state: State,
+) => (input: Input) => StateMachineTransition<State, Effect> | undefined;
+
+export type StateMachineTransitions<T extends StateMachine<any, any, any>> =
+	T extends StateMachine<infer S, infer I, infer E>
+		? StateMachineTransitionMap<S, I, E>
+		: never;
+
+export class StateMachine<
+	State extends StateMachineState,
+	Input extends StateMachineInput,
+	Effect = undefined,
+> {
+	public constructor(
+		initialState: State,
+		transitions: StateMachineTransitionMap<State, Input, Effect>,
+	) {
+		this._initial = this._state = initialState;
+		this.transitions = transitions;
+	}
+
+	protected transitions: StateMachineTransitionMap<State, Input, Effect>;
+
+	/** Restarts the machine from the initial state */
+	public restart(): void {
+		this._state = this._initial;
+	}
+
+	/** Determines the next transition to take */
+	public next(
+		input: Input,
+	): StateMachineTransition<State, Effect> | undefined {
+		if (this._state.done) return;
+		return this.transitions(this._state)(input);
+	}
+
+	/** Transitions the machine to the next state. This does not execute effects */
+	public transition(next?: State): void {
+		// Allow some convenience by passing the transition's next state directly
+		if (next == undefined) return;
+		this._state = next;
+	}
+
+	private _initial: State;
+	private _state: State;
+	/** Returns the current state of the state machine */
+	public get state(): State {
+		return this._state;
+	}
+
+	/** Returns whether this state machine is done */
+	public get done(): boolean {
+		return !!this._state.done;
+	}
+}
