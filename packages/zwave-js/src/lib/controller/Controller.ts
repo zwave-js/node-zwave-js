f2c151c553c876db96dc76635a87cc41293cc3cc
AlCalzone <dominic.griesel@nabucasa.com>
2025-01-30 08:43:33 +0100

    refactor: improve tree-shaking for logging code (#7582)

--- packages/zwave-js/src/lib/controller/Controller.ts
+++ packages/zwave-js/src/lib/controller/Controller.ts
@@ -362,7 +362,6 @@ import {
 	areUint8ArraysEqual,
 	cloneDeep,
 	createThrowingMap,
-	flatMap,
 	getEnumMemberName,
 	getErrorMessage,
 	noop,
@@ -5074,13 +5073,11 @@ export class ZWaveController
 				let associatedNodes: number[] = [];
 				try {
 					associatedNodes = distinct(
-						flatMap<number, AssociationAddress[]>(
-							[
-								...(self.getAssociations({ nodeId: node.id })
-									.values() as any),
-							],
-							(assocs: AssociationAddress[]) =>
-								assocs.map((a) => a.nodeId),
+						[
+							...(self.getAssociations({ nodeId: node.id })
+								.values() as any),
+						].flatMap((assocs: AssociationAddress[]) =>
+							assocs.map((a) => a.nodeId)
 						),
 					)
 						// ...except the controller itself, which was handled by step 2
