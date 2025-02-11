994162bcb9097ed267e7ed9eccf1df7c6514b556
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-02-11 14:24:51 +0100

    feat: add supportsNLS flag to KEX Report command

--- packages/zwave-js/src/lib/controller/Controller.ts
+++ packages/zwave-js/src/lib/controller/Controller.ts
@@ -3781,6 +3781,7 @@ export class ZWaveController
 			}
 			// Confirm the keys - the node will start requesting the granted keys in response
 			await api.confirmRequestedKeys({
+				supportsNLS: kexParams.supportsNLS,
 				requestCSA: kexParams.requestCSA,
 				requestedKeys: [...kexParams.requestedKeys],
 				supportedECDHProfiles: [...kexParams.supportedECDHProfiles],
@@ -9313,6 +9314,7 @@ export class ZWaveController
 			// Send with our desired keys
 			await api.requestKeys({
 				requestedKeys: requested.securityClasses,
+				supportsNLS: true,
 				requestCSA: false,
 				supportedECDHProfiles: [ECDHProfiles.Curve25519],
 				supportedKEXSchemes: [KEXSchemes.KEXScheme1],
